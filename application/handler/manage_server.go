4d0b19c73ed7b45755977d1f8427b2f29acf71b2
Wenhui Shen <swhbox@foxmail.com>
2016-12-03 16:30:42 +0800

    update

--- application/handler/manage_server.go
+++ application/handler/manage_server.go
@@ -1,7 +1,8 @@
 package handler
 
 import (
-	"time"
+	"os/exec"
+	"strings"
 
 	"github.com/admpub/log"
 	"github.com/admpub/sockjs-go/sockjs"
@@ -9,36 +10,76 @@ import (
 	"github.com/webx-top/echo"
 )
 
+type CMDResultCapturer struct {
+	Do func([]byte) error
+}
+
+func (this CMDResultCapturer) Write(p []byte) (n int, err error) {
+	err = this.Do(p)
+	n = len(p)
+	return
+}
+
 func ManageExeCMD(ctx echo.Context) error {
 	var err error
 	return ctx.Render(`manage/execmd`, err)
 }
 
+func runCMD(command string, recvResult func([]byte) error) {
+	params := strings.Split(command, ` `)
+	length := len(params)
+	if len(params[0]) == 0 {
+		return
+	}
+	var cmd *exec.Cmd
+	if length > 1 {
+		cmd = exec.Command(params[0], params[1:]...)
+	} else {
+		cmd = exec.Command(params[0])
+	}
+	out := CMDResultCapturer{Do: recvResult}
+	cmd.Stdout = out
+	cmd.Stderr = out
+
+	go func() {
+		err := cmd.Run()
+		if err != nil {
+			recvResult([]byte(err.Error()))
+		}
+	}()
+}
+
 func SockJSManageExeCMDSend(c sockjs.Session) error {
+	send := make(chan func() string)
 	//push(writer)
 	go func() {
-		var counter int
 		for {
-			time.Sleep(5 * time.Second)
-			message := time.Now().String()
+			fn := <-send
+			message := fn()
 			log.Info(`Push message: `, message)
 			if err := c.Send(message); err != nil {
 				log.Error(`Push error: `, err.Error())
 				return
 			}
-			counter++
 		}
 	}()
 
 	//echo
 	var execute = func(session sockjs.Session) error {
 		for {
-			msg, err := session.Recv()
-			log.Info(`Recv message: `, msg)
+			command, err := session.Recv()
 			if err != nil {
 				return err
 			}
-			err = session.Send(msg)
+			if len(command) > 0 {
+				runCMD(command, func(b []byte) error {
+					send <- func() string {
+						return string(b)
+					}
+					return nil
+				})
+			}
+			err = session.Send(command)
 			if err != nil {
 				return err
 			}
@@ -52,18 +93,17 @@ func SockJSManageExeCMDSend(c sockjs.Session) error {
 }
 
 func WSManageExeCMDSend(c *websocket.Conn, ctx echo.Context) error {
+	send := make(chan func() string)
 	//push(writer)
 	go func() {
-		var counter int
 		for {
-			time.Sleep(5 * time.Second)
-			message := time.Now().String()
+			fn := <-send
+			message := fn()
 			log.Info(`Push message: `, message)
 			if err := c.WriteMessage(websocket.TextMessage, []byte(message)); err != nil {
 				log.Error(`Push error: `, err.Error())
 				return
 			}
-			counter++
 		}
 	}()
 
@@ -74,7 +114,15 @@ func WSManageExeCMDSend(c *websocket.Conn, ctx echo.Context) error {
 			if err != nil {
 				return err
 			}
-			log.Infof("Websocket recv: %s", message)
+			command := string(message)
+			if len(command) > 0 {
+				runCMD(command, func(b []byte) error {
+					send <- func() string {
+						return string(b)
+					}
+					return nil
+				})
+			}
 
 			if err = conn.WriteMessage(mt, message); err != nil {
 				return err
