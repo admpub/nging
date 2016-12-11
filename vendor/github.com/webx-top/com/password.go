d7136bc33a824f9a602f47134cb1ab7fba2a40b7
Wenhui Shen <swhbox@foxmail.com>
2016-12-11 18:00:08 +0800

    update

--- vendor/github.com/webx-top/com/password.go
+++ vendor/github.com/webx-top/com/password.go
@@ -17,22 +17,128 @@
 */
 package com
 
-// 生成哈希值
+import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"hash"
+)
+
+// Hash 生成哈希值
 func Hash(str string) string {
 	return Sha256(str)
 }
 
-// 盐值加密
+// Salt 盐值加密
 func Salt() string {
 	return Hash(RandStr(64))
 }
 
-// 创建密码
-func MakePassword(password string, salt string) string {
-	return Hash(salt + password)
+// MakePassword 创建密码
+func MakePassword(password string, salt string, positions ...uint) string {
+	length := len(positions)
+	if length < 1 {
+		return Hash(salt + password)
+	}
+	saltLength := len(salt)
+	if saltLength < length {
+		return Hash(salt + password)
+	}
+	saltChars := saltLength / length
+	hashedPassword := Hash(password)
+	maxIndex := len(hashedPassword) - 1
+	saltMaxIndex := saltLength - 1
+	var result string
+	var lastPos int
+	for k, pos := range positions {
+		end := int(pos)
+		start := lastPos
+		if start > end {
+			start, end = end, start
+		}
+		if start > maxIndex {
+			continue
+		}
+		lastPos = end
+		saltStart := k * saltChars
+		saltEnd := saltStart + saltChars
+		if end > maxIndex {
+			result += hashedPassword[start:] + salt[saltStart:saltEnd]
+			continue
+		}
+		result += hashedPassword[start:end] + salt[saltStart:saltEnd]
+		if k == length-1 {
+			if end <= maxIndex {
+				result += hashedPassword[end:]
+			}
+			if saltEnd <= saltMaxIndex {
+				result += salt[saltEnd:]
+			}
+		}
+	}
+	return result
 }
 
-// 检查密码(密码原文，数据库中保存的哈希过后的密码，数据库中保存的盐值)
-func CheckPassword(rawPassword string, hashedPassword string, salt string) bool {
-	return MakePassword(rawPassword, salt) == hashedPassword
+// CheckPassword 检查密码(密码原文，数据库中保存的哈希过后的密码，数据库中保存的盐值)
+func CheckPassword(rawPassword string, hashedPassword string, salt string, positions ...uint) bool {
+	return MakePassword(rawPassword, salt, positions...) == hashedPassword
+}
+
+// Key derives a key from the password, salt and iteration count, returning a
+// []byte of length keylen that can be used as cryptographic key. The key is
+// derived based on the method described as PBKDF2 with the HMAC variant using
+// the supplied hash function.
+//
+// For example, to use a HMAC-SHA-1 based PBKDF2 key derivation function, you
+// can get a derived key for e.g. AES-256 (which needs a 32-byte key) by
+// doing:
+//
+// 	dk := pbkdf2.Key([]byte("some password"), salt, 4096, 32, sha1.New)
+//
+// Remember to get a good random salt. At least 8 bytes is recommended by the
+// RFC.
+//
+// Using a higher iteration count will increase the cost of an exhaustive
+// search but will also make derivation proportionally slower.
+func PBKDF2Key(password, salt []byte, iter, keyLen int, hFunc ...func() hash.Hash) string {
+	var h func() hash.Hash
+	if len(hFunc) > 0 {
+		h = hFunc[0]
+	}
+	if h == nil {
+		h = sha1.New
+	}
+	prf := hmac.New(h, password)
+	hashLen := prf.Size()
+	numBlocks := (keyLen + hashLen - 1) / hashLen
+
+	var buf [4]byte
+	dk := make([]byte, 0, numBlocks*hashLen)
+	U := make([]byte, hashLen)
+	for block := 1; block <= numBlocks; block++ {
+		// N.B.: || means concatenation, ^ means XOR
+		// for each block T_i = U_1 ^ U_2 ^ ... ^ U_iter
+		// U_1 = PRF(password, salt || uint(i))
+		prf.Reset()
+		prf.Write(salt)
+		buf[0] = byte(block >> 24)
+		buf[1] = byte(block >> 16)
+		buf[2] = byte(block >> 8)
+		buf[3] = byte(block)
+		prf.Write(buf[:4])
+		dk = prf.Sum(dk)
+		T := dk[len(dk)-hashLen:]
+		copy(U, T)
+
+		// U_n = PRF(password, U_(n-1))
+		for n := 2; n <= iter; n++ {
+			prf.Reset()
+			prf.Write(U)
+			U = U[:0]
+			U = prf.Sum(U)
+			for x := range U {
+				T[x] ^= U[x]
+			}
+		}
+	}
+	return Base64Encode(string(dk[:keyLen]))
 }
