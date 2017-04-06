f66bb6c88bbd5083c4303f532f2e0c4eb46cd1bc
swh <swhbox@foxmail.com>
2017-04-06 11:17:22 +0800

    增加定时任务和用户表

--- config/install.sql
+++ config/install.sql
@@ -1,4 +1,4 @@
--- Adminer 4.2.5 MySQL dump
+-- Adminer 4.2.3 MySQL dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -37,6 +37,83 @@ CREATE TABLE `ftp_user_group` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='FTP用户组';
 
 
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `group_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '分组ID',
+  `name` varchar(50) NOT NULL DEFAULT '' COMMENT '任务名称',
+  `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '任务类型',
+  `description` varchar(200) NOT NULL DEFAULT '' COMMENT '任务描述',
+  `cron_spec` varchar(100) NOT NULL DEFAULT '' COMMENT '时间表达式',
+  `concurrent` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否只允许一个实例',
+  `command` text NOT NULL COMMENT '命令详情',
+  `disabled` enum('Y','N') NOT NULL DEFAULT 'N' COMMENT '是否禁用',
+  `enable_notify` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否启用通知',
+  `notify_email` text NOT NULL COMMENT '通知人列表',
+  `timeout` smallint(6) unsigned NOT NULL DEFAULT '0' COMMENT '超时设置',
+  `execute_times` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '累计执行次数',
+  `prev_time` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '上次执行时间',
+  `created` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_uid` (`uid`),
+  KEY `idx_group_id` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务';
+
+
+DROP TABLE IF EXISTS `task_group`;
+CREATE TABLE `task_group` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户ID',
+  `group_name` varchar(50) NOT NULL DEFAULT '' COMMENT '组名',
+  `description` varchar(255) NOT NULL DEFAULT '' COMMENT '说明',
+  `created` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_uid` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务组';
+
+
+DROP TABLE IF EXISTS `task_log`;
+CREATE TABLE `task_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `task_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '任务ID',
+  `output` mediumtext NOT NULL COMMENT '任务输出',
+  `error` text NOT NULL COMMENT '错误信息',
+  `status` enum('success','timeout','failure','stop','restart') NOT NULL DEFAULT 'success' COMMENT '状态',
+  `elapsed` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '消耗时间(毫秒)',
+  `created` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_task_id` (`task_id`,`created`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务日志';
+
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL DEFAULT '' COMMENT '用户名',
+  `email` varchar(50) NOT NULL DEFAULT '' COMMENT '邮箱',
+  `password` char(40) NOT NULL DEFAULT '' COMMENT '密码',
+  `salt` char(40) NOT NULL DEFAULT '' COMMENT '盐值',
+  `last_login` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '最后登录时间',
+  `last_ip` varchar(150) NOT NULL DEFAULT '' COMMENT '最后登录IP',
+  `disabled` enum('Y','N') NOT NULL DEFAULT 'N' COMMENT '状态',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
+
+
+DROP TABLE IF EXISTS `user_u2f`;
+CREATE TABLE `user_u2f` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` int(11) unsigned NOT NULL COMMENT '用户ID',
+  `token` varchar(255) NOT NULL COMMENT '签名',
+  `type` varchar(30) NOT NULL COMMENT '类型',
+  `extra` text NOT NULL COMMENT '扩展设置',
+  `created` int(11) unsigned NOT NULL COMMENT '绑定时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='两步验证';
+
+
 DROP TABLE IF EXISTS `vhost`;
 CREATE TABLE `vhost` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -50,4 +127,4 @@ CREATE TABLE `vhost` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='虚拟主机';
 
 
--- 2016-12-11 11:16:40
+-- 2017-04-06 03:15:48
