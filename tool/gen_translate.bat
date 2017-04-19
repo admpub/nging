4d405b22036e74b17302a9243a645d63c9cdaee5
Wenhui Shen <swhbox@foxmail.com>
2017-04-20 00:57:37 +0800

    双语支持

--- tool/gen_translate.bat
+++ tool/gen_translate.bat
@@ -1,3 +1,3 @@
 go get github.com/admpub/i18n/cmd/fetchtext
-fetchtext --src=..\ --dist=..\config\i18n\messages --default=zh-cn
+fetchtext --src=..\ --dist=..\config\i18n\messages --default=zh-cn --translate=true
 pause
