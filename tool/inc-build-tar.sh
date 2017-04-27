a15a96b80a0129a63332e02ed2c85248c2db418b
swh <swhbox@foxmail.com>
2017-04-27 18:13:30 +0800

    update

--- tool/inc-build-tar.sh
+++ tool/inc-build-tar.sh
@@ -3,10 +3,12 @@ mkdir ../dist/nging_${GOOS}_${GOARCH}
 go build -tags "bindata sqlite" -o ../dist/nging_${GOOS}_${GOARCH}/nging_${GOOS}_${GOARCH}${NGINGEX} ..
 cp -R ../data ../dist/nging_${GOOS}_${GOARCH}/data
 
+mkdir ../dist/nging_${GOOS}_${GOARCH}/vhost
+mkdir ../dist/nging_${GOOS}_${GOARCH}/config
+
 cp -R ../config/config.yaml ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml
 cp -R ../config/config.yaml.sample ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml.sample
 cp -R ../config/install.sql ../dist/nging_${GOOS}_${GOARCH}/config/install.sql
-cp -R ../config/vhost ../dist/nging_${GOOS}_${GOARCH}/config/vhost
 
 cp -R ../dist/default/* ../dist/nging_${GOOS}_${GOARCH}/
 cd ../dist/nging_${GOOS}_${GOARCH}
