1bef27dd0b4d174aead7d2f515ddfe103fc35818
swh <swhbox@foxmail.com>
2017-04-27 15:40:23 +0800

    fixed bug

--- tool/inc-build-tar.sh
+++ tool/inc-build-tar.sh
@@ -3,7 +3,8 @@ mkdir ../dist/nging_${GOOS}_${GOARCH}
 go build -tags "bindata" -o ../dist/nging_${GOOS}_${GOARCH}/nging_${GOOS}_${GOARCH}${NGINGEX} ..
 cp -R ../data ../dist/nging_${GOOS}_${GOARCH}/data
 cp -R ../config ../dist/nging_${GOOS}_${GOARCH}/config
-cp -R ../dist/default ../dist/nging_${GOOS}_${GOARCH}/
-
-tar -zcvf ../dist/nging_${GOOS}_${GOARCH}.tar.gz ../dist/nging_${GOOS}_${GOARCH}/*
+cp -R ../dist/default/* ../dist/nging_${GOOS}_${GOARCH}/
+cd ../dist/nging_${GOOS}_${GOARCH}
+tar -zcvf ../nging_${GOOS}_${GOARCH}.tar.gz ./*
+cd ../../tool
 rm -rf ../dist/nging_${GOOS}_${GOARCH}
