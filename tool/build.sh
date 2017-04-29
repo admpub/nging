e1860149c8f3a5bec53caa8cedbbc8d69eb50474
Hank Shen <swhbox@foxmail.com>
2017-04-30 00:50:53 +0800

    update

---
+++ tool/build.sh
@@ -0,0 +1,20 @@
+
+# set GOPATH GOROOT
+export GOPATH="/home/admpub/go"
+export GOROOT="/usr/local/go"
+
+# set PATH so it includes user's private bin directories
+export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+
+go get github.com/jteeuwen/go-bindata/...
+go get github.com/admpub/go-bindata-assetfs/...
+cd ..
+$GOPATH/bin/go-bindata-assetfs -tags bindata public/... template/... config/i18n/...
+cd tool
+
+export NGINGEX=
+
+export GOOS=linux
+export GOARCH=amd64
+source ${PWD}/inc-build-tar.sh
+
