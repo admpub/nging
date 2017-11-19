bb8d374a7099c062526dbf489b4354acf417398e
Hank Shen <swhbox@foxmail.com>
2017-11-19 21:24:45 +0800

    update

--- application/handler/task/email.go
+++ application/handler/task/email.go
@@ -30,7 +30,7 @@ func EmailTest(ctx echo.Context) error {
 		toEmail := ctx.Form(`email`)
 		toUsername := `test`
 		title := ctx.T(`恭喜！邮件发送功能正常`)
-		content := []byte(ctx.T(`如果您收到这封邮件，说明邮件发送功能正常。<br /><br /> 来自：%s<br />时间：%s`, ctx.Site(), time.Now().String()))
+		content := []byte(ctx.T(`如果您收到这封邮件，说明邮件发送功能正常。<br /><br /> 来自：%s<br />时间：%s`, ctx.Site(), time.Now().Format(time.RFC3339)))
 		err := cron.SendMail(toEmail, toUsername, title, content)
 		data := ctx.Data()
 		if err != nil {
