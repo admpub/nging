815b50d0048b154b04b2c9a3e51d2f6343f1c491
HankShen <swhbox@foxmail.com>
2018-05-11 20:12:28 +0800

    update

---
+++ tool/build-by-gox.sh
@@ -0,0 +1,34 @@
+go get github.com/mitchellh/gox
+go get github.com/jteeuwen/go-bindata/...
+go get github.com/admpub/go-bindata-assetfs/...
+cd ..
+$GOPATH/bin/go-bindata-assetfs -tags bindata public/... template/... config/i18n/...
+cd tool
+
+export NGINGEX=
+export BUILDTAGS=
+
+export GOOS=linux
+export GOARCH=amd64
+source ${PWD}/inc-build-gox.sh
+
+
+export GOOS=linux
+export GOARCH=386
+source ${PWD}/inc-build-gox.sh
+
+export GOOS=darwin
+export GOARCH=amd64
+source ${PWD}/inc-build-gox.sh
+
+
+
+export NGINGEX=.exe
+
+export GOOS=windows
+export GOARCH=386
+source ${PWD}/inc-build-gox.sh
+
+export GOOS=windows
+export GOARCH=amd64
+source ${PWD}/inc-build-gox.sh
---
+++ tool/inc-build-gox.sh
@@ -0,0 +1,22 @@
+mkdir ../dist/nging_${GOOS}_${GOARCH}
+gox -tags "bindata sqlite" -osarch ${GOOS}/${GOARCH} -output ../dist/nging_${GOOS}_${GOARCH}/nging${NGINGEX} ..
+cp -R ../data ../dist/nging_${GOOS}_${GOARCH}/data
+
+mkdir ../dist/nging_${GOOS}_${GOARCH}/config
+mkdir ../dist/nging_${GOOS}_${GOARCH}/config/vhosts
+
+cp -R ../config/config.yaml ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml
+cp -R ../config/config.yaml.sample ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml.sample
+cp -R ../config/install.sql ../dist/nging_${GOOS}_${GOARCH}/config/install.sql
+
+if [ $GOOS = "windows" ]; then
+	export archiver_extension=zip
+else
+	export archiver_extension=tar.bz2
+fi
+
+cp -R ../dist/default/* ../dist/nging_${GOOS}_${GOARCH}/
+
+archiver make ../nging_${GOOS}_${GOARCH}.${archiver_extension} ../dist/nging_${GOOS}_${GOARCH}/
+
+rm -rf ../dist/nging_${GOOS}_${GOARCH}
