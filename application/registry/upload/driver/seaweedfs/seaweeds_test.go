d21cb7b7f9be7d7f6f107aa3babd54f1f29e243c
Hank Shen <swhbox@foxmail.com>
2019-05-14 00:04:13 +0800

    update

--- application/registry/upload/driver/seaweedfs/seaweeds_test.go
+++ application/registry/upload/driver/seaweedfs/seaweeds_test.go
@@ -38,9 +38,9 @@ import (
 //取消挂载：关闭服务后执行 sudo umount -l ./public/upload/test
 
 // SeaweedFS对图片有很好的支持，可以指定图片显示的长度、宽度、模式，如：
-// http://localhost:8888/3/01637037d6.jpg?height=200&width=200
-// http://localhost:8888/3/01637037d6.jpg?height=200&width=200&mode=fit
-// http://localhost:8888/3/01637037d6.jpg?height=200&width=200&mode=fill
+// http://localhost:8989/3/01637037d6.jpg?height=200&width=200
+// http://localhost:8989/3/01637037d6.jpg?height=200&width=200&mode=fit
+// http://localhost:8989/3/01637037d6.jpg?height=200&width=200&mode=fill
 
 func TestSeaweedfs(t *testing.T) {
 	return
