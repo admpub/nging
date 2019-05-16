d33c9174833e78d39cade028e4eaa3570a9fbb3d
Hank Shen <swhbox@foxmail.com>
2019-05-16 22:22:11 +0800

    update

--- tool/inc-build-x.sh
+++ tool/inc-build-x.sh
@@ -1,5 +1,6 @@
 mkdir ../dist/nging_${GOOS}_${GOARCH}
-xgo -go=1.12 -image=admpub/xgo:latest -targets=${GOOS}/${GOARCH} -dest=../dist/nging_${GOOS}_${GOARCH} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ../
+xgo -go=1.12 -image=admpub/xgo:master -targets=${GOOS}/${GOARCH} -dest=../dist/nging_${GOOS}_${GOARCH} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ../
+mv ../dist/nging_${GOOS}_${GOARCH}/nging-${GOOS}-${GOARCH} ../dist/nging_${GOOS}_${GOARCH}/nging${NGINGEX}
 mkdir ../dist/nging_${GOOS}_${GOARCH}/data
 mkdir ../dist/nging_${GOOS}_${GOARCH}/data/logs
 cp -R ../data/ip2region ../dist/nging_${GOOS}_${GOARCH}/data/ip2region
@@ -15,7 +16,7 @@ cp -R ../config/ua.txt ../dist/nging_${GOOS}_${GOARCH}/config/ua.txt
 export archiver_extension=zip
 
 cp -R ../dist/default/* ../dist/nging_${GOOS}_${GOARCH}/
-
-archiver make ../dist/nging_v${NGING_VERSION}_${GOOS}_${GOARCH}.${archiver_extension} ../dist/nging_${GOOS}_${GOARCH}/
+#${NGING_VERSION}${NGING_LABEL}
+archiver make ../dist/nging_${GOOS}_${GOARCH}.${archiver_extension} ../dist/nging_${GOOS}_${GOARCH}/
 
 rm -rf ../dist/nging_${GOOS}_${GOARCH}
