43d2b371f57e82da09740a935a6ee5a9bf7a5ff9
Hank Shen <swhbox@foxmail.com>
2019-06-09 13:26:30 +0800

    beta5

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ var (
 	BUILD_TIME string
 	CLOUD_GOX  string
 	COMMIT     string
-	LABEL      = `beta4` //beta/alpha/stable
+	LABEL      = `beta5` //beta/alpha/stable
 	VERSION    = `2.0.0`
 
 	version   string
