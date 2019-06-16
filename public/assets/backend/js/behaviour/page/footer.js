5983c82dcef469d9a8dfd4f7f7968f838f15003e
Hank Shen <swhbox@foxmail.com>
2019-06-16 21:24:59 +0800

    update

--- public/assets/backend/js/behaviour/page/footer.js
+++ public/assets/backend/js/behaviour/page/footer.js
@@ -7,4 +7,18 @@ $(function(){
     if(window.errorMSG) App.message({title: App.i18n.SYS_INFO, text: window.errorMSG, class_name: "danger"});
 	if(window.successMSG) App.message({title: App.i18n.SYS_INFO, text: window.successMSG, class_name: "success"});
 	App.notifyListen();
+	$('#topnav a[data-project]').on('click',function(e){
+		e.preventDefault();
+		var ident=$(this).data('project');
+		if(ident==$('#leftnav').data('project')) return;
+		$.get(window.BACKEND_URL+'/project/'+ident,{partial:1},function(r){
+			if(r.Code!=1){
+				App.message({title:App.i18n.SYS_INFO,text:r.Info,type:'error'});
+				return;
+			}
+			$('#leftnav').html(r.Data.list);
+			App.initLeftNav();
+			App.initLeftNavAjax(window.activeURL,'#leftnav');
+		},'html');
+	});
 });
