72ca1628f3eb88060638dda1683573b0c47b5a6d
Hank Shen <swhbox@foxmail.com>
2019-07-10 22:32:03 +0800

    update

--- application/library/natss/natss.go
+++ application/library/natss/natss.go
@@ -17,3 +17,51 @@
 */
 
 package natss
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+
+	natsd "github.com/nats-io/nats-server/v2/server"
+	stand "github.com/nats-io/nats-streaming-server/server"
+	stores "github.com/nats-io/nats-streaming-server/stores"
+	"github.com/webx-top/echo"
+)
+
+func Start() {
+	// This configure the NATS Server using natsd package
+	nOpts := &natsd.Options{
+		HTTPPort: 8222,
+		Port:     4223,
+	}
+	/*/ Create the NATS Server
+	  ns := natsd.New(nOpts)
+
+	  // Start it as a go routine
+	  go ns.Start()
+
+	  // Wait for it to be able to accept connections
+	  if !ns.ReadyForConnections(10 * time.Second) {
+	    panic("not able to start")
+	  }
+	  // */
+
+	// Get NATS Streaming Server default options
+	sOpts := stand.GetDefaultOptions()
+	sOpts.StoreType = stores.TypeFile
+	sOpts.FilestoreDir = filepath.Join(echo.Wd(), `data`, `nats`)
+	// Force the streaming server to setup its own signal handler
+	sOpts.HandleSignals = true
+	// override the NoSigs for NATS since Streaming has its own signal handler
+	nOpts.NoSigs = true
+	// Without this option set to true, the logger is not configured.
+	sOpts.EnableLogging = true
+	// This will invoke RunServerWithOpts but on Windows, may run it as a service.
+	if _, err := stand.Run(sOpts, nOpts); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	runtime.Goexit()
+}
