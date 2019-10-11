31512ee9520eb5faf4f31404094f378f23e2a96b
Hank Shen <swhbox@foxmail.com>
2019-10-11 21:19:27 +0800

    update

--- application/registry/upload/dbsaver.go
+++ application/registry/upload/dbsaver.go
@@ -3,8 +3,9 @@ package upload
 import (
 	"io"
 
-	modelFile "github.com/admpub/nging/application/model/file"
 	uploadClient "github.com/webx-top/client/upload"
+
+	modelFile "github.com/admpub/nging/application/model/file"
 )
 
 type (
@@ -20,6 +21,15 @@ var (
 
 func DBSaverRegister(key string, dbsaver DBSaver) {
 	dbSavers[key] = dbsaver
+}
+
+func DBSaverUnregister(keys ...string) {
+	for _, key := range keys {
+		_, ok := dbSavers[key]
+		if ok {
+			delete(dbSavers, key)
+		}
+	}
 }
 
 func DBSaverGet(key string) DBSaver {
