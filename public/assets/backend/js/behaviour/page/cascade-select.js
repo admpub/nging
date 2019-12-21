4ad6cbde63355a6acc0fc77ddf1cb9156b38f1fa
Hank Shen <swhbox@foxmail.com>
2019-12-21 18:35:57 +0800

    update

---
+++ public/assets/backend/js/behaviour/page/cascade-select.js
@@ -0,0 +1,55 @@
+
+(function(win){
+function fetchOptions(elem,pid,selectedId,selectedIds,url){
+  if(!url) url=BACKEND_URL+'/tool/area/index';
+  $.get(url,{pid:pid},function(r){
+      if(r.Code!=1){
+        return App.message({text:r.Info,type:'error'});
+      }
+      if(r.Data.listData.length<1){
+        while($(elem).next('select').length>0) $(elem).next('select').remove();
+        $(elem).remove();
+        return;
+      }
+      var h = '<option value=""> - '+(App.i18n.PLEASE_SELECT?App.i18n.PLEASE_SELECT:'请选择')+' - </option>';
+      for(var i=0;i<r.Data.listData.length;i++){
+        var v=r.Data.listData[i];
+        var s=selectedId==v.id?' selected':'';
+        h+='<option value="'+v.id+'"'+s+'>'+v.name+'</option>';
+      }
+      $(elem).html(h);
+      bindEvent(elem,selectedIds,url).trigger('change');
+  },'json');
+}
+function bindEvent(elem,selectedIds,url){
+  if($(elem).data('bindEvent')){
+    return $(elem);
+  }
+  $(elem).data('bindEvent',true);
+  return $(elem).on('change',function(){
+      var v=$(this).val();
+      var n=$(this).attr('name');
+      var c=$(this).attr('class');
+      var p=Number($(this).attr('pos')||0);
+      if(v==''||v=='0'){
+        while($(elem).next('select').length>0) $(elem).next('select').remove();
+        return;
+      }
+      var index=p+1;
+      if($(this).next('select').length<1){
+        var props = ' pos="'+index+'"';
+        if(n) props+=' name="'+n+'"';
+        if(c) props+=' class="'+c+'"';
+        $(this).after('<select'+props+'></select>');
+      }
+      var selectedId='';
+      if($.isArray(selectedIds) && selectedIds.length>index) selectedId=selectedIds[index];
+      fetchOptions($(this).next('select')[0],v,selectedId,selectedIds,url);
+  });
+}
+win.CascadeSelect = {
+  init: function(elem,selectedIds,url){
+    fetchOptions(elem,0,selectedIds?selectedIds[0]:'',selectedIds,url);
+  }
+};
+})(window);
