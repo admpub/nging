d4aa30e9c1b2b379f8759285c797c5b21ea88adb
Hank Shen <swhbox@foxmail.com>
2020-02-27 17:48:56 +0800

    improved

--- tool/inc-build-x.sh
+++ tool/inc-build-x.sh
@@ -1,23 +1,23 @@
-mkdir ../dist/nging_${GOOS}_${GOARCH}
-xgo -go=1.13.3 -image=admpub/xgo -targets=${GOOS}/${GOARCH} -dest=../dist/nging_${GOOS}_${GOARCH} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ../
-mv ../dist/nging_${GOOS}_${GOARCH}/nging-${GOOS}-* ../dist/nging_${GOOS}_${GOARCH}/nging${NGINGEX}
-mkdir ../dist/nging_${GOOS}_${GOARCH}/data
-mkdir ../dist/nging_${GOOS}_${GOARCH}/data/logs
-cp -R ../data/ip2region ../dist/nging_${GOOS}_${GOARCH}/data/ip2region
+mkdir ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}
+xgo -go=1.13.3 -image=admpub/xgo -targets=${GOOS}/${GOARCH} -dest=../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ../
+mv ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/${NGING_EXECUTOR}-${GOOS}-* ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/${NGING_EXECUTOR}${NGINGEX}
+mkdir ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data
+mkdir ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data/logs
+cp -R ../data/ip2region ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data/ip2region
 
-mkdir ../dist/nging_${GOOS}_${GOARCH}/config
-mkdir ../dist/nging_${GOOS}_${GOARCH}/config/vhosts
+mkdir ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config
+mkdir ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/vhosts
 
-#cp -R ../config/config.yaml ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml
-cp -R ../config/config.yaml.sample ../dist/nging_${GOOS}_${GOARCH}/config/config.yaml.sample
-cp -R ../config/install.* ../dist/nging_${GOOS}_${GOARCH}/config/
-cp -R ../config/preupgrade.* ../dist/nging_${GOOS}_${GOARCH}/config/
-cp -R ../config/ua.txt ../dist/nging_${GOOS}_${GOARCH}/config/ua.txt
+#cp -R ../config/config.yaml ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/config.yaml
+cp -R ../config/config.yaml.sample ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/config.yaml.sample
+cp -R ../config/install.* ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/
+cp -R ../config/preupgrade.* ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/
+cp -R ../config/ua.txt ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/ua.txt
 
 export archiver_extension=zip
 
-cp -R ../dist/default/* ../dist/nging_${GOOS}_${GOARCH}/
+cp -R ../dist/default/* ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/
 #${NGING_VERSION}${NGING_LABEL}
-archiver make ../dist/nging_${GOOS}_${GOARCH}.${archiver_extension} ../dist/nging_${GOOS}_${GOARCH}/
+archiver make ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}.${archiver_extension} ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/
 
-rm -rf ../dist/nging_${GOOS}_${GOARCH}
+rm -rf ../dist/${NGING_EXECUTOR}_${GOOS}_${GOARCH}
