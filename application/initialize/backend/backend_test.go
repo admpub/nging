90458e0abbe63333a1f6a89942f5c4260e8cfc37
Hank Shen <swhbox@foxmail.com>
2020-02-29 19:36:46 +0800

    improved

--- application/initialize/backend/backend_test.go
+++ application/initialize/backend/backend_test.go
@@ -10,5 +10,5 @@ func TestMakeSubdomains(t *testing.T) {
 	r := MakeSubdomains(`www.coscms.com,www.coscms.com:8181`, []string{})
 	test.Eq(t, `www.coscms.com,www.coscms.com:8181,www.coscms.com:0`, r)
 	r = MakeSubdomains(`www.coscms.com,www.coscms.com:8181`, DefaultLocalHostNames)
-	test.Eq(t, `www.coscms.com,www.coscms.com:8181,www.coscms.com:0,127.0.0.1:0,localhost:0`, r)
+	test.Eq(t, `www.coscms.com,www.coscms.com:8181,www.coscms.com:0,127.0.0.1:0,127.0.0.1:8181,localhost:0,localhost:8181`, r)
 }
