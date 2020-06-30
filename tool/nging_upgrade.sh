0ecc0e2c8404904e85c361459a38968ac3b3cb62
Hank Shen <swhbox@foxmail.com>
2020-06-30 21:00:57 +0800

    update

--- tool/nging_upgrade.sh
+++ tool/nging_upgrade.sh
@@ -7,6 +7,6 @@ cp /home/www/eget_download/nging/latest/nging_linux_amd64.zip ./
 #remote
 #wget https://dl.eget.io/nging/latest/nging_linux_amd64.zip
 
-unzip nging_linux_amd64.zip
+unzip nging_linux_amd64.zip -d ./nging_linux_amd64
 rm nging_linux_amd64.zip
 service Nging start
