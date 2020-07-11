213e01397901a5992d8d751a0a4ab6f6cc547a16
Hank Shen <swhbox@foxmail.com>
2020-07-11 19:24:26 +0800

    update

--- application/library/config/cliconfig.go
+++ application/library/config/cliconfig.go
@@ -31,9 +31,9 @@ import (
 	"github.com/admpub/log"
 	"github.com/admpub/nging/application/cmd/event"
 	"github.com/admpub/nging/application/library/caddy"
+	"github.com/admpub/nging/application/library/config/startup"
 	"github.com/admpub/nging/application/library/cron"
 	"github.com/admpub/nging/application/library/frp"
-	"github.com/admpub/nging/application/library/config/startup"
 	"github.com/spf13/pflag"
 	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
@@ -63,8 +63,8 @@ func (c *CLIConfig) InitFlag(flagSet *pflag.FlagSet) {
 	flagSet.StringVarP(&c.Confx, `subconfig`, `u`, filepath.Join(echo.Wd(), `config/config.frpserver.yaml`), `submodule config`)
 	flagSet.StringVarP(&c.Type, `type`, `t`, `manager`, `operation type`)
 	flagSet.StringVarP(&c.Startup, `startup`, `s`, `webserver,task,daemon,ftpserver,frpserver,frpclient`, `startup`)
-	flagSet.StringVarP(&c.FrontendDomain, `frontenddomain`, `f`, ``, `frontend domain`)
-	flagSet.StringVarP(&c.BackendDomain, `backenddomain`, `b`, ``, `backend domain`)
+	flagSet.StringVarP(&c.FrontendDomain, `frontend.domain`, `f`, ``, `frontend domain`)
+	flagSet.StringVarP(&c.BackendDomain, `backend.domain`, `b`, ``, `backend domain`)
 }
 
 func (c *CLIConfig) OnlyRunServer() bool {
