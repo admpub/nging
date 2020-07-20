fbaa3843395eabb9c8ab30ca5ad25dfa2beeadb6
Hank Shen <swhbox@foxmail.com>
2020-07-20 19:10:45 +0800

    update

--- application/registry/upload/checker/checkerFn/checker.go
+++
@@ -1,9 +0,0 @@
-package checkerFn
-
-import (
-	"github.com/admpub/nging/application/registry/upload/table"
-	"github.com/webx-top/echo"
-)
-
-// Checker 验证并生成子文件夹名称和文件名称
-type Checker func(echo.Context, table.TableInfoStorer) (subdir string, name string, err error)
