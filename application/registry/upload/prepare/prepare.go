c96f0c5226773fea84c55b9b84178d3bd117a8d8
Hank Shen <swhbox@foxmail.com>
2020-07-20 20:07:07 +0800

    update

--- application/registry/upload/prepare/prepare.go
+++ application/registry/upload/prepare/prepare.go
@@ -10,6 +10,7 @@ import (
 	uploadSubdir "github.com/admpub/nging/application/registry/upload/subdir"
 	uploadClient "github.com/webx-top/client/upload"
 	"github.com/webx-top/echo"
+	"github.com/webx-top/echo/code"
 )
 
 var NopChecker uploadClient.Checker = func(r *uploadClient.Result) error {
@@ -32,7 +33,7 @@ type PrepareData struct {
 func (p *PrepareData) Storer(ctx echo.Context) (driver.Storer, error) {
 	var err error
 	if p.storer == nil {
-		p.storer, err = p.newStorer(ctx, p.TableName) // 使用表名称作为文件夹名
+		p.storer, err = p.newStorer(ctx, p.Subdir) // 使用表名称作为文件夹名
 	}
 	return p.storer, err
 }
@@ -47,16 +48,16 @@ func (p *PrepareData) Close() error {
 // Prepare 上传前的环境准备
 func Prepare(ctx echo.Context, uploadType string, fileType string, storerInfo storer.Info) (*PrepareData, error) {
 	if len(uploadType) == 0 {
-		return nil, ctx.E(`请提供参数“%s”`, ctx.Path())
+		return nil, ctx.NewError(code.InvalidParameter, `请提供参数“%s”`, ctx.Path())
 	}
 	params := uploadSubdir.ParseUploadType(uploadType)
 	if !params.IsAllowed() {
-		return nil, ctx.E(`参数“%s”未被登记`, uploadType)
+		return nil, ctx.NewError(code.InvalidParameter, `参数“%s”未被登记`, uploadType)
 	}
 	//echo.Dump(ctx.Forms())
 	newStore := driver.Get(storerInfo.Name)
 	if newStore == nil {
-		return nil, ctx.E(`存储引擎“%s”未被登记`, storerInfo.Name)
+		return nil, ctx.NewError(code.InvalidParameter, `存储引擎“%s”未被登记`, storerInfo.Name)
 	}
 	dbSaverKey := params.MustGetTable()
 	if len(params.Field) > 0 {
@@ -67,7 +68,7 @@ func Prepare(ctx echo.Context, uploadType string, fileType string, storerInfo st
 		extension := path.Ext(r.FileName)
 		if len(r.FileType) > 0 {
 			if !uploadClient.CheckTypeExtension(fileType, extension) {
-				return ctx.E(`不支持将扩展名为“%v”的文件作为“%v”类型的文件来进行上传`, extension, fileType)
+				return ctx.NewError(code.InvalidParameter, `不支持将扩展名为“%v”的文件作为“%v”类型的文件来进行上传`, extension, fileType)
 			}
 		} else {
 			r.FileType = uploadClient.FileType(uploadClient.DetectType(extension))
