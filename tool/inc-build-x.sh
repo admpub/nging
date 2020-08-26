a965f9b9a7df70e3a7c6cc7a6a2481fe36195571
Hank Shen <swhbox@foxmail.com>
2020-08-26 13:05:39 +0800

    update

--- tool/inc-build-x.sh
+++ tool/inc-build-x.sh
@@ -1,27 +1,29 @@
 export DISTPATH=${PKGPATH}/dist
-mkdir ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}
-xgo -go=latest -goproxy=https://goproxy.cn,direct -image=admpub/xgo -targets=${GOOS}/${GOARCH} -dest=${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ./${PKGPATH}
-mv ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/${NGING_EXECUTOR}-${GOOS}-* ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/${NGING_EXECUTOR}${NGINGEX}
-mkdir ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data
-mkdir ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data/logs
-cp -R ${PKGPATH}/data/ip2region ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/data/ip2region
+export RELEASEDIR=${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}
+mkdir ${RELEASEDIR}
+xgo -go=latest -goproxy=https://goproxy.cn,direct -image=admpub/xgo -targets=${GOOS}/${GOARCH} -dest=${RELEASEDIR} -out=${NGING_EXECUTOR} -tags="bindata sqlite${BUILDTAGS}" -ldflags="-X main.BUILD_TIME=${NGING_BUILD} -X main.COMMIT=${NGING_COMMIT} -X main.VERSION=${NGING_VERSION} -X main.LABEL=${NGING_LABEL}" ./${PKGPATH}
 
-mkdir ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config
-mkdir ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/vhosts
+mv ${RELEASEDIR}/${NGING_EXECUTOR}-${GOOS}-* ${RELEASEDIR}/${NGING_EXECUTOR}${NGINGEX}
+mkdir ${RELEASEDIR}/data
+mkdir ${RELEASEDIR}/data/logs
+cp -R ${PKGPATH}/data/ip2region ${RELEASEDIR}/data/ip2region
 
-#cp -R ../config/config.yaml ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/config.yaml
-cp -R ${PKGPATH}/config/config.yaml.sample ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/config.yaml.sample
-cp -R ${PKGPATH}/config/install.* ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/
-cp -R ${PKGPATH}/config/preupgrade.* ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/
-cp -R ${PKGPATH}/config/ua.txt ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/config/ua.txt
+mkdir ${RELEASEDIR}/config
+mkdir ${RELEASEDIR}/config/vhosts
+
+#cp -R ../config/config.yaml ${RELEASEDIR}/config/config.yaml
+cp -R ${PKGPATH}/config/config.yaml.sample ${RELEASEDIR}/config/config.yaml.sample
+cp -R ${PKGPATH}/config/install.* ${RELEASEDIR}/config/
+cp -R ${PKGPATH}/config/preupgrade.* ${RELEASEDIR}/config/
+cp -R ${PKGPATH}/config/ua.txt ${RELEASEDIR}/config/ua.txt
 
 export archiver_extension=zip
 
-cp -R ${DISTPATH}/default/* ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/
+cp -R ${DISTPATH}/default/* ${RELEASEDIR}/
 
-rm -rf ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}.${archiver_extension}
+rm -rf ${RELEASEDIR}.${archiver_extension}
 
 #${NGING_VERSION}${NGING_LABEL}
-arc archive ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}.${archiver_extension} ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}/
+arc archive ${RELEASEDIR}.${archiver_extension} ${RELEASEDIR}/
 
-rm -rf ${DISTPATH}/${NGING_EXECUTOR}_${GOOS}_${GOARCH}
+rm -rf ${RELEASEDIR}
