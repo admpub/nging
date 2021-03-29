6e3ecca274b8b0f1a89efd4ec54e9bcbf707fbc0
Hank Shen <swhbox@foxmail.com>
2021-03-29 16:44:45 +0800

    增加分片及其合并文件管理

--- application/registry/upload/chunk/chunk.go
+++ application/registry/upload/chunk/chunk.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	uploadClient "github.com/webx-top/client/upload"
-	"github.com/webx-top/echo"
 )
 
 var (
@@ -20,17 +19,25 @@ var (
 	// MergeSaveDir 分片文件合并后保存的临时文件夹
 	// 避免和最终的文件保存位置相同，上传后一般需要将合并后的文件转移(os.Rename)到最终保存位置
 	MergeSaveDir = filepath.Join(os.TempDir(), `nging/chunk_merged`)
+
+	// TempLifetime 分片临时文件过期时间
+	TempLifetime = 24 * time.Hour
+
+	// GCInterval 垃圾清理间隔时间
+	GCInterval = 48 * time.Hour
 )
 
 func ChunkUploader() uploadClient.ChunkUpload {
-	chunkUploadInitOnce.Do(func() {
-		chunkUpload = &uploadClient.ChunkUpload{
-			TempDir:      ChunkTempDir,
-			SaveDir:      MergeSaveDir,
-			TempLifetime: 24 * time.Hour,
-		}
-		echo.Dump(chunkUpload)
-		go chunkUpload.StartGC(48 * time.Hour)
-	})
+	chunkUploadInitOnce.Do(initChunkUploader)
 	return *chunkUpload
+}
+
+func initChunkUploader() {
+	chunkUpload = &uploadClient.ChunkUpload{
+		TempDir:      ChunkTempDir,
+		SaveDir:      MergeSaveDir,
+		TempLifetime: TempLifetime,
+	}
+	//echo.Dump(chunkUpload)
+	go chunkUpload.StartGC(GCInterval)
 }
