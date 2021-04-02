5b3e6e2801605f4631009c5be0176235f78790f8
Hank Shen <swhbox@foxmail.com>
2021-04-02 17:15:04 +0800

    update

--- tool/build-by-xgo.sh
+++ tool/build-by-xgo.sh
@@ -10,50 +10,128 @@ go generate
 cd ${ENTRYDIR}
 
 #echo $PWD && exit
+reset() {
+    export TMPDIR=
+    export NGINGEX=
+    export BUILDTAGS=
+}
 
-export TMPDIR=
+all() {
+    linux_amd64
+    linux_arm5
+    linux_arm6
+    linux_arm7
+    linux_arm64
+    linux_386
+    darwin_amd64
+    windows_386
+    windows_amd64
+}
 
-export NGINGEX=
-export BUILDTAGS=
+build() {
+    case "$1" in
+        "linux_amd64")
+            linux_amd64
+            ;;
+        "linux_arm5")
+            linux_arm5
+            ;;
+        "linux_arm6")
+            linux_arm6
+            ;;
+        "linux_arm7")
+            linux_arm7
+            ;;
+        "linux_arm64")
+            linux_arm64
+            ;;
+        "linux_386")
+            linux_386
+            ;;
+        "darwin_amd64")
+            darwin_amd64
+            ;;
+        "windows_386")
+            windows_386
+            ;;
+        "windows_amd64")
+            windows_amd64
+            ;;
+        *)
+            all
+    esac
+}
 
-export GOOS=linux
-export GOARCH=amd64
-source ${WORKDIR}/inc-build-x.sh
+linux_amd64() {
+    export NGINGEX=
+    export GOOS=linux
+    export GOARCH=amd64
+    source ${WORKDIR}/inc-build-x.sh
+}
 
-#exit;
+linux_arm5() {
+    export NGINGEX=
 
-export GOOS=linux
-export GOARCH=arm-5
-source ${WORKDIR}/inc-build-x.sh
+    export GOOS=linux
+    export GOARCH=arm-5
+    source ${WORKDIR}/inc-build-x.sh
+}
 
-export GOOS=linux
-export GOARCH=arm-6
-source ${WORKDIR}/inc-build-x.sh
+linux_arm6() {
+    export NGINGEX=
 
-export GOOS=linux
-export GOARCH=arm-7
-source ${WORKDIR}/inc-build-x.sh
+    export GOOS=linux
+    export GOARCH=arm-6
+    source ${WORKDIR}/inc-build-x.sh
+}
 
-export GOOS=linux
-export GOARCH=arm64
-source ${WORKDIR}/inc-build-x.sh
+linux_arm7() {
+    export NGINGEX=
 
-export GOOS=linux
-export GOARCH=386
-source ${WORKDIR}/inc-build-x.sh
+    export GOOS=linux
+    export GOARCH=arm-7
+    source ${WORKDIR}/inc-build-x.sh
+}
 
-export GOOS=darwin
-export GOARCH=amd64
-source ${WORKDIR}/inc-build-x.sh
+linux_arm64() {
+    export NGINGEX=
 
+    export GOOS=linux
+    export GOARCH=arm64
+    source ${WORKDIR}/inc-build-x.sh
+}
 
+linux_386() {
+    export NGINGEX=
 
-export NGINGEX=.exe
+    export GOOS=linux
+    export GOARCH=386
+    source ${WORKDIR}/inc-build-x.sh
+}
 
-export GOOS=windows
-export GOARCH=386
-source ${WORKDIR}/inc-build-x.sh
+darwin_amd64() {
+    export NGINGEX=
 
-export GOOS=windows
-export GOARCH=amd64
-source ${WORKDIR}/inc-build-x.sh
+    export GOOS=darwin
+    export GOARCH=amd64
+    source ${WORKDIR}/inc-build-x.sh
+}
+
+windows_386() {
+    export NGINGEX=.exe
+
+    export GOOS=windows
+    export GOARCH=386
+    source ${WORKDIR}/inc-build-x.sh
+}
+
+windows_amd64() {
+    export NGINGEX=.exe
+
+    export GOOS=windows
+    export GOARCH=amd64
+    source ${WORKDIR}/inc-build-x.sh
+}
+
+reset
+build
