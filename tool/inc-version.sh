3130477ebe9245bb428fce2fa8301cce4b58c004
Hank Shen <swhbox@foxmail.com>
2021-05-01 16:45:42 +0800

    v3.4.3

--- tool/inc-version.sh
+++ tool/inc-version.sh
@@ -2,7 +2,7 @@ export PKGPATH=github.com/admpub/nging
 export ENTRYDIR=${GOPATH}/src
 export WORKDIR=${PWD}
 
-export NGING_VERSION="3.4.2"
+export NGING_VERSION="3.4.3"
 export NGING_BUILD=`date +%Y%m%d%H%M%S`
 export NGING_COMMIT=`git rev-parse HEAD`
 export NGING_LABEL="stable"
