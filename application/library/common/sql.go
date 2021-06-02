6b85515e7287d0ba3e7e25764d72fed6b1d6ab2f
Hank Shen <swhbox@foxmail.com>
2021-06-02 11:02:33 +0800

    update

--- application/library/common/sql.go
+++ application/library/common/sql.go
@@ -7,7 +7,9 @@ import (
 	"github.com/webx-top/com"
 	"github.com/webx-top/db"
 	"github.com/webx-top/db/lib/factory"
+	mysqlUtil "github.com/webx-top/db/lib/factory/mysql"
 	"github.com/webx-top/db/mysql"
+	"github.com/webx-top/echo"
 )
 
 func SQLLineParser(exec func(string) error) func(string) error {
@@ -85,5 +87,43 @@ func ParseMysqlConnectionURL(settings *mysql.ConnectionURL) {
 		settings.Socket = strings.TrimPrefix(settings.Host, `unix:`)
 		settings.Socket = mysqlNetworkRegexp.ReplaceAllString(settings.Socket, `/`)
 		settings.Host = ``
+	}
+}
+
+func SelectPageCond(ctx echo.Context, cond *db.Compounds, pkAndLabelFields ...string) {
+	pk := `id`
+	lb := `name`
+	switch len(pkAndLabelFields) {
+	case 2:
+		if len(pkAndLabelFields[1]) > 0 {
+			lb = pkAndLabelFields[1]
+		}
+		fallthrough
+	case 1:
+		if len(pkAndLabelFields[0]) > 0 {
+			pk = pkAndLabelFields[0]
+		}
+	}
+	searchValue := ctx.Formx(`searchValue`).String()
+	if len(searchValue) > 0 {
+		cond.AddKV(pk, searchValue)
+	} else {
+		keywords := ctx.FormValues(`q_word[]`)
+		andOr := ctx.Form(`andOr`)
+		var sep string
+		if len(andOr) == 0 || andOr == `AND` {
+			sep = ` `
+		} else {
+			sep = `||`
+		}
+		q := strings.Join(keywords, `"`+sep+`"`)
+		if len(q) == 0 {
+			q = ctx.Formx(`q`).String()
+		} else {
+			q = `"` + q + `"`
+		}
+		if len(q) > 0 {
+			cond.From(mysqlUtil.SearchField(lb, q, pk))
+		}
 	}
 }
