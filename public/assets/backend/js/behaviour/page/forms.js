8b737b6c7b06876e92b80ec7dfa2be97cb1d1d74
Hank Shen <swhbox@foxmail.com>
2021-06-06 17:42:46 +0800

    update

--- public/assets/backend/js/behaviour/page/forms.js
+++ public/assets/backend/js/behaviour/page/forms.js
@@ -1,7 +1,7 @@
 (function(){
 var initFlag = 'data-initialized';
 function initForms(formElem, urlPrefix) {
-   if(formElem == null) formElem = 'form';
+   if(formElem == null) formElem = 'form[generator="forms"]';
    if(urlPrefix == null) urlPrefix = BACKEND_URL;
    $(formElem).find('.form-selectpage:not(['+initFlag+'=true])').each(function(){
     $(this).attr(initFlag,'true');
@@ -12,6 +12,11 @@ function initForms(formElem, urlPrefix) {
     $(this).attr(initFlag,'true');
     if(!$(this).data('url')) $(this).data('url',urlPrefix+$(this).attr('url'));
     App.editor.cascadeSelect(this);
+  });
+  $(formElem).each(function(){
+    if($(this).data('parsley-initialized')) return;
+    $(this).data('parsley-initialized',true);
+    $(this).parsley();
   });
 }
 window.initForms = initForms;
