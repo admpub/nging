ecd71d34ad84bf8152d9cf09c98ea4ca49f8a802
Hank Shen <swhbox@foxmail.com>
2021-06-24 13:27:04 +0800

    update

--- application/registry/perm/behavior.go
+++ application/registry/perm/behavior.go
@@ -4,6 +4,8 @@ import "github.com/admpub/nging/application/library/perm"
 
 var Behaviors = perm.NewBehaviors()
 
+/*
 func init() {
 	Behaviors.Register(`test`, `测试行为`, perm.BehaviorOptFormHelpBlock(`仅仅用于开发测试`), perm.BehaviorOptValue(`默认值`))
 }
+*/
