c6f89a0e3bebb9802be8f4ff3a67351bc0002cf6
Hank Shen <swhbox@foxmail.com>
2021-07-03 16:30:43 +0800

    update

--- application/library/common/config.go
+++ application/library/common/config.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"strings"
+
 	"github.com/webx-top/echo"
 
 	"github.com/admpub/nging/application/library/config/subconfig/scookie"
@@ -21,4 +23,18 @@ func CookieConfig() scookie.Config {
 
 func Setting(group ...string) echo.H {
 	return echo.GetStoreByKeys(`NgingConfig`, group...)
+}
+
+func BackendURL(ctx echo.Context) string {
+	backendURL := Setting(`base`).String(`backendURL`)
+	if len(backendURL) == 0 {
+		backendURL = ctx.Site()
+	}
+	backendURL = strings.TrimSuffix(backendURL, `/`)
+	return backendURL
+}
+
+func APIKey(ctx echo.Context) string {
+	apiKey := Setting(`base`).String(`apiKey`)
+	return apiKey
 }
