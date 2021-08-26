99c475811ba6d356c3cf1d89ee83f438db9f7fc9
Hank Shen <swhbox@foxmail.com>
2021-08-26 19:11:19 +0800

    update

--- application/library/flock/helper.go
+++ application/library/flock/helper.go
@@ -10,8 +10,8 @@ import (
 // IsCompleted 等待文件有数据且已写完
 // 费时操作 放在子线程中执行
 // @param file  文件
-// @param start 需要传入 System.currentTimeMillis()，用于兼容遍历的情况
-// @return true：已写完 false:外部程序阻塞或者文件不存在
+// @param start 需要传入 time.Now.Local()，用于兼容遍历的情况
+// @return true:已写完 false:外部程序阻塞或者文件不存在
 // 翻译自：https://blog.csdn.net/northernice/article/details/115986671
 func IsCompleted(file *os.File, start time.Time) bool {
 	var (
