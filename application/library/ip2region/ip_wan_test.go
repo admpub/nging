90156194a1432c45010fc7e4e4b3b4a719b35cd2
Wenhui Shen <swhbox@foxmail.com>
2021-09-01 22:02:53 +0800

    update

--- application/library/ip2region/ip_wan_test.go
+++ application/library/ip2region/ip_wan_test.go
@@ -1,12 +1,31 @@
 package ip2region
 
-import "testing"
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/com"
+)
 
 func TestIPWAN(t *testing.T) {
-	wan, err := GetWANIP()
+	wan, err := GetWANIP(true)
 	if err != nil {
 		t.Fatal(err)
 	}
-	t.Log(wan.IPv4)
-	t.Log(wan.IPv6)
+	fmt.Println(`IPv4:`, wan.IPv4)
+	fmt.Println(`IPv6:`, wan.IPv6)
+	r := regexp.MustCompile(IPv6Rule)
+	matches := r.FindAllStringSubmatch(` 	2001:3CA1:010F:001A:121B:0000:0000:0010`, 1)
+	com.Dump(matches)
+	expected := [][]string{
+		[]string{
+			"2001:3CA1:010F:001A:121B:0000:0000:0010",
+			"2001:3CA1:010F:001A:121B:0000:0000:0010",
+			"", "",
+		},
+	}
+	assert.Equal(t, expected, matches)
+	//panic(``)
 }
