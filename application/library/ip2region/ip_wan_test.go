67ac813d97b6bcc724826aa4e88972ba1ad4a535
Hank Shen <swhbox@foxmail.com>
2021-09-04 16:23:00 +0800

    improved

--- application/library/ip2region/ip_wan_test.go
+++ application/library/ip2region/ip_wan_test.go
@@ -2,7 +2,6 @@ package ip2region
 
 import (
 	"fmt"
-	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +15,7 @@ func TestIPWAN(t *testing.T) {
 	}
 	fmt.Println(`IPv4:`, wan.IPv4)
 	fmt.Println(`IPv6:`, wan.IPv6)
-	r := regexp.MustCompile(IPv6Rule)
-	matches := r.FindAllStringSubmatch(` 	2001:3CA1:010F:001A:121B:0000:0000:0010`, 1)
+	matches := ipv6Regexp.FindAllStringSubmatch(` 	2001:3CA1:010F:001A:121B:0000:0000:0010`, 1)
 	com.Dump(matches)
 	expected := [][]string{
 		[]string{
