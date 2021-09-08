861cb2209131395435c52fcd729da8ce6d11a12a
Hank Shen <swhbox@foxmail.com>
2021-09-08 11:37:07 +0800

    v3.6.1

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	CLOUD_GOX  string
 	COMMIT     string
 	LABEL      = `dev` //beta/alpha/stable
-	VERSION    = `3.6.1`
+	VERSION    = `3.6.2`
 	PACKAGE    = `free`
 
 	schemaVer = version.DBSCHEMA //数据表结构版本
