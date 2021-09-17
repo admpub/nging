24ff17fe9ea488d7f5e731e75820dca66327575e
Hank Shen <swhbox@foxmail.com>
2021-09-17 14:14:04 +0800

    update

--- application/registry/navigate/collection.go
+++ application/registry/navigate/collection.go
@@ -27,9 +27,48 @@ func (n *Navigates) Add(typ NavigateType, nav *List) {
 	(*n)[typ] = nav
 }
 
+func (n *Navigates) AddItems(typ NavigateType, index int, items ...*Item) {
+	nav := n.Get(typ)
+	if nav != nil {
+		nav.Add(index, items...)
+	}
+}
+
+func (n *Navigates) AddTopItems(index int, items ...*Item) {
+	n.AddItems(Top, index, items...)
+}
+
+func (n *Navigates) AddLeftItems(index int, items ...*Item) {
+	n.AddItems(Left, index, items...)
+}
+
+func (n *Navigates) AddRightItems(index int, items ...*Item) {
+	n.AddItems(Right, index, items...)
+}
+
+func (n *Navigates) AddBottomItems(index int, items ...*Item) {
+	n.AddItems(Bottom, index, items...)
+}
+
 func (n *Navigates) Get(typ NavigateType) (nav *List) {
 	nav = (*n)[typ]
 	return
+}
+
+func (n *Navigates) GetTop() *List {
+	return n.Get(Top)
+}
+
+func (n *Navigates) GetLeft() *List {
+	return n.Get(Left)
+}
+
+func (n *Navigates) GetRight() *List {
+	return n.Get(Right)
+}
+
+func (n *Navigates) GetBottom() *List {
+	return n.Get(Bottom)
 }
 
 func (n *Navigates) Remove(typ NavigateType) bool {
