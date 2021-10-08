5b89445d166c5fed04892510210430a1b1f33b76
Hank Shen <swhbox@foxmail.com>
2021-10-08 12:30:43 +0800

    update

--- application/handler/manager/cache.go
+++ application/handler/manager/cache.go
@@ -35,7 +35,7 @@ func ClearCache(ctx echo.Context) error {
 }
 
 func ReloadEnv(ctx echo.Context) error {
-	if err := config.DefaultCLIConfig.InitEnviron(); err != nil {
+	if err := config.DefaultCLIConfig.InitEnviron(true); err != nil {
 		return err
 	}
 	return ctx.String(ctx.T(`重载完毕`))
