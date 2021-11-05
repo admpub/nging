7b62f6d7b594c3ea05408ed41f620932b43a11c0
Wenhui Shen <swhbox@foxmail.com>
2021-11-05 21:51:51 +0800

    update

--- vendor/github.com/webx-top/db/lib/factory/cacher.go
+++ vendor/github.com/webx-top/db/lib/factory/cacher.go
@@ -3,6 +3,6 @@ package factory
 type Cacher interface {
 	Put(key string, value interface{}, ttlSeconds int64) error
 	Del(key string) error
-	Get(key string) (interface{}, error)
+	Get(key string, recv interface{}) error
 	Do(key string, recv interface{}, fn func() error, ttlSeconds int64) error
 }
