3114910be107472081439d78ea3782cf46bacb45
Hank Shen <swh@admpub.com>
2021-11-15 15:26:22 +0800

    update

--- application/library/s3manager/s3manager_test.go
+++ application/library/s3manager/s3manager_test.go
@@ -13,11 +13,13 @@ import (
 func TestStat(t *testing.T) {
 	return
 	cfg := &dbschema.NgingCloudStorage{
-		Key:    os.Getenv(`S3_KEY`),
-		Secret: os.Getenv(`S3_SECRET`),
-		Secure: `Y`,
-		Region: os.Getenv(`S3_REGION`),
-		Bucket: os.Getenv(`S3_BUCKET`),
+		Key:      os.Getenv(`S3_KEY`),
+		Secret:   os.Getenv(`S3_SECRET`),
+		Secure:   `Y`,
+		Region:   os.Getenv(`S3_REGION`),
+		Bucket:   os.Getenv(`S3_BUCKET`),
+		Endpoint: os.Getenv(`S3_ENDPOINT`),
+		Baseurl:  os.Getenv(`S3_BASEURL`),
 	}
 	mgr, err := s3client.New(cfg, 1024000)
 	if err != nil {
