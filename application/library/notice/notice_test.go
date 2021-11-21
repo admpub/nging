129d87258956327a90896fc6e5da33aece3ce4f6
Hank Shen <swh@admpub.com>
2021-11-21 21:47:52 +0800

    update

--- application/library/notice/notice_test.go
+++ application/library/notice/notice_test.go
@@ -19,8 +19,6 @@
 package notice
 
 import (
-	"strconv"
-	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -41,48 +39,4 @@ func TestOpenMessage(t *testing.T) {
 
 	CloseClient(`testUser`, clientID)
 	assert.Equal(t, 0, user.CountClient())
-}
-
-func TestSend(t *testing.T) {
-	OpenMessage(`testUser`, `testType`)
-	_, clientID := OpenClient(`testUser`)
-	var wg sync.WaitGroup
-	go func() {
-		defer wg.Done()
-		b, err := RecvJSON(`testUser`, clientID)
-		if err != nil {
-			t.Error(err)
-		}
-		println(string(b))
-		assert.Equal(t, `{"client_id":"`+clientID+`","id":null,"type":"testType","title":"testTitle","status":1,"content":"testContent","mode":"","progress":null}`, string(b))
-	}()
-	wg.Add(1)
-	Send(`testUser`, NewMessageWithValue(`testType`, `testTitle`, `testContent`).SetClientID(clientID))
-	wg.Wait()
-}
-
-func TestSend2(t *testing.T) {
-	OpenMessage(`testUser2`, `testType`)
-	_, clientID := OpenClient(`testUser2`)
-	var wg sync.WaitGroup
-	go func() {
-		var i = 0
-		for {
-			b, err := RecvJSON(`testUser`, clientID)
-			if err != nil {
-				t.Error(err)
-			}
-			println(string(b))
-			assert.Equal(t, `{"client_id":"`+clientID+`","id":null,"type":"testType","title":"testTitle","status":1,"content":"testContent_`+strconv.Itoa(i)+`","mode":"","progress":null}`, string(b))
-			i++
-		}
-	}()
-	for i := 0; i < 50; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			Send(`testUser`, NewMessageWithValue(`testType`, `testTitle`, `testContent_`+strconv.Itoa(i)).SetClientID(clientID))
-		}(i)
-	}
-	wg.Wait()
 }
