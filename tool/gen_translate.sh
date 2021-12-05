00092753820d70db6ecbc6b6acb6293d72b6095f
Hank Shen <swh@admpub.com>
2021-12-05 15:22:25 +0800

    update

--- tool/gen_translate.sh
+++ tool/gen_translate.sh
@@ -1,2 +1,2 @@
-go get github.com/admpub/i18n/cmd/fetchtext
-fetchtext --src=../ --dist=../config/i18n/messages --default=zh-cn --translate=true
+#go install github.com/admpub/i18n/cmd/fetchtext@latest
+fetchtext --src=../ --dist=../config/i18n/messages --default=zh-cn --translate=true --translator=baidu --translatorConfig="appid=&secret="
