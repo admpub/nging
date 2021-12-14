69897af478b8e5871bd7428bc421119f2993d9c4
Hank Shen <swh@admpub.com>
2021-12-14 13:03:03 +0800

    update

---
+++ tool/pushAllPlugins.sh
@@ -0,0 +1,14 @@
+pwd=`pwd`/../../../nging-plugins
+dir=.
+filelist=`ls $dir`
+for file in $filelist; do
+	    echo $pwd/$file
+    if test -d $file; then
+	    echo $pwd/$file
+        cd $pwd/$file
+        git add .
+        git commit -m update
+        git push
+        cd $pwd
+    fi
+done
