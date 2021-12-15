9d249954036302205356f152ea752e84fd1f99f2
Hank Shen <swh@admpub.com>
2021-12-15 20:29:33 +0800

    update

--- bindata_none.go
+++ bindata_none.go
@@ -25,16 +25,18 @@ import (
 	"github.com/admpub/nging/v4/application/library/bindata"
 )
 
+const NgingPluginDir = `../../nging-plugins`
+
 func init() {
-	bindata.PathAliases.Add(`caddy`, `../../nging-plugins/caddymanager/template/backend`)
-	bindata.PathAliases.Add(`collector`, `../../nging-plugins/collector/template/backend`)
-	bindata.PathAliases.Add(`db`, `../../nging-plugins/dbmanager/template/backend`)
-	bindata.PathAliases.Add(`ddns`, `../../nging-plugins/ddnsmanager/template/backend`)
-	bindata.PathAliases.Add(`download`, `../../nging-plugins/dlmanager/template/backend`)
-	bindata.PathAliases.Add(`frp`, `../../nging-plugins/frpmanager/template/backend`)
-	bindata.PathAliases.Add(`ftp`, `../../nging-plugins/ftpmanager/template/backend`)
-	bindata.PathAliases.Add(`server`, `../../nging-plugins/servermanager/template/backend`)
-	bindata.PathAliases.Add(`term`, `../../nging-plugins/sshmanager/template/backend`)
+	bindata.PathAliases.Add(`caddy`, NgingPluginDir+`/caddymanager/template/backend`)
+	bindata.PathAliases.Add(`collector`, NgingPluginDir+`/collector/template/backend`)
+	bindata.PathAliases.Add(`db`, NgingPluginDir+`/dbmanager/template/backend`)
+	bindata.PathAliases.Add(`ddns`, NgingPluginDir+`/ddnsmanager/template/backend`)
+	bindata.PathAliases.Add(`download`, NgingPluginDir+`/dlmanager/template/backend`)
+	bindata.PathAliases.Add(`frp`, NgingPluginDir+`/frpmanager/template/backend`)
+	bindata.PathAliases.Add(`ftp`, NgingPluginDir+`/ftpmanager/template/backend`)
+	bindata.PathAliases.Add(`server`, NgingPluginDir+`/servermanager/template/backend`)
+	bindata.PathAliases.Add(`term`, NgingPluginDir+`/sshmanager/template/backend`)
 
 	bindata.Initialize()
 }
