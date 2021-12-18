bfd9d50eb1f97037685446804f3196b9b8b2a02f
Hank Shen <swh@admpub.com>
2021-12-18 10:57:11 +0800

    update

--- tool/pluginsPushAll.sh
+++ tool/pluginsPushAll.sh
@@ -5,7 +5,7 @@ for file in $filelist; do
     if test -d $pwd/$file; then
 	    echo "push: $pwd/$file"
         cd $pwd/$file
-        go mod tidy -compat=1.7
+        go mod tidy -compat=1.17
         git add .
         git commit -m update
         git push
