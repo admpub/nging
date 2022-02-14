d3cd8dfbfc3b643783c6365537eb3d3b1a2fe300
Hank Shen <swh@admpub.com>
2022-02-14 18:57:15 +0800

    update

--- application/library/module/module_bindata.go
+++ application/library/module/module_bindata.go
@@ -3,19 +3,11 @@
 
 package module
 
-import (
-	"github.com/admpub/nging/v4/application/library/bindata"
-	"github.com/admpub/nging/v4/application/library/ntemplate"
-	"github.com/webx-top/echo/middleware"
-)
-
 func (m *Module) applyTemplateAndAssets() {
 }
 
 func SetBackendTemplate(key string, templatePath string) {
-	SetTemplate(bindata.PathAliases, key, templatePath)
 }
 
 func SetBackendAssets(assetsPath string) {
-	SetAssets(bindata.StaticOptions, assetsPath)
 }
