d7704ef83d62658ee1357e835a4222e5860eddb3
Hank Shen <swh@admpub.com>
2022-06-11 00:30:24 +0800

    update

--- application/registry/upload/chunk/chunk.go
+++ application/registry/upload/chunk/chunk.go
@@ -1,12 +1,12 @@
 package chunk
 
 import (
-	"os"
 	"path/filepath"
 	"time"
 
 	syncOnce "github.com/admpub/once"
 	uploadClient "github.com/webx-top/client/upload"
+	"github.com/webx-top/echo"
 )
 
 var (
@@ -14,11 +14,11 @@ var (
 	chunkUpload         *uploadClient.ChunkUpload
 
 	// ChunkTempDir 保存分片文件的临时文件夹
-	ChunkTempDir = filepath.Join(os.TempDir(), `nging/chunk_temp`)
+	ChunkTempDir = filepath.Join(echo.Wd(), `data/temp/upload/chunk_temp`)
 
 	// MergeSaveDir 分片文件合并后保存的临时文件夹
 	// 避免和最终的文件保存位置相同，上传后一般需要将合并后的文件转移(os.Rename)到最终保存位置
-	MergeSaveDir = filepath.Join(os.TempDir(), `nging/chunk_merged`)
+	MergeSaveDir = filepath.Join(echo.Wd(), `data/temp/upload/chunk_merged`)
 
 	// TempLifetime 分片临时文件过期时间
 	TempLifetime = 24 * time.Hour
