76f6ad1c539e6e564851ec45cdbe82231ac22233
Hank Shen <swh@admpub.com>
2022-08-21 02:48:10 +0800

    fix

--- application/library/sessionguard/sessionguard.go
+++ application/library/sessionguard/sessionguard.go
@@ -26,10 +26,6 @@ func GetLastLoginInfo(ctx echo.Context, ownerType string, ownerId uint64, sessio
 // Validate 验证 session 环境是否安全，避免 cookie 和 session id 被窃取
 // 在非匿名模式下 UserAgent 和 IP 归属地与登录时的一致
 func Validate(ctx echo.Context, lastIP string, ownerType string, ownerId uint64) bool {
-	currentIP := ctx.RealIP()
-	if lastIP == currentIP {
-		return true
-	}
 	k := `backend.Anonymous`
 	if ownerType != `user` {
 		k = `frontend.Anonymous`
@@ -39,16 +35,15 @@ func Validate(ctx echo.Context, lastIP string, ownerType string, ownerId uint64)
 	}
 	info, err := GetLastLoginInfo(ctx, ownerType, ownerId, ctx.Session().MustID())
 	if err != nil {
-		log.Errorf(`GetLastLoginInfo: %v`, err)
+		log.Errorf(`failed to GetLastLoginInfo: %v`, err)
 		return false
 	}
 	if info.UserAgent != ctx.Request().UserAgent() {
 		return false
 	}
-	ipInfo, err := ip2region.IPInfo(currentIP)
-	if err != nil {
-		log.Errorf(`IPInfo: %v`, err)
-		return false
+	currentIP := ctx.RealIP()
+	if lastIP == currentIP {
+		return true
 	}
 	if len(info.IpLocation) == 0 {
 		return false
@@ -57,6 +52,11 @@ func Validate(ctx echo.Context, lastIP string, ownerType string, ownerId uint64)
 	err = json.Unmarshal([]byte(info.IpLocation), &ipLoc)
 	if err != nil {
 		log.Errorf(`failed to unmarshal IpLocation: %v`, err)
+		return false
+	}
+	ipInfo, err := ip2region.IPInfo(currentIP)
+	if err != nil {
+		log.Errorf(`failed to get IPInfo: %v`, err)
 		return false
 	}
 	if ipInfo.Country == `0` {
