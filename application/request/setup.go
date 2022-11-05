76147f39f1265e63836a23a150d9d5298235c4af
Hank Shen <swh@admpub.com>
2022-11-05 14:26:38 +0800

    update

--- application/request/setup.go
+++ application/request/setup.go
@@ -3,6 +3,7 @@ package request
 import (
 	"strings"
 
+	"github.com/admpub/nging/v5/application/library/config"
 	"github.com/admpub/nging/v5/application/library/config/subconfig/sdb"
 	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
@@ -12,18 +13,21 @@ import (
 var _ echo.AfterValidate = &Setup{}
 
 type Setup struct {
-	Type       string `validate:"oneof='mysql' 'sqlite'"`
-	User       string `validate:""`
-	Password   string `validate:""`
-	Host       string `validate:"omitempty,hostname"`
-	Database   string `validate:"omitempty,alphanum_"`
-	Charset    string `validate:""`
+	Type       string
+	User       string
+	Password   string
+	Host       string
+	Database   string
+	Charset    string `validate:"omitempty,alphanum"`
 	AdminUser  string `validate:"username"`
 	AdminPass  string `validate:"required,min=8,max=64"`
 	AdminEmail string `validate:"required,email"`
 }
 
 func (s *Setup) AfterValidate(c echo.Context) error {
+	if _, ok := config.DBInstallers[s.Type]; !ok {
+		return c.NewError(code.InvalidParameter, `不支持的数据库类型: %v`, s.Type).SetZone(`type`)
+	}
 	if s.Type == `sqlite` {
 		s.User = ``
 		s.Password = ``
@@ -36,7 +40,7 @@ func (s *Setup) AfterValidate(c echo.Context) error {
 			s.Charset = sdb.MySQLDefaultCharset
 		} else {
 			if !com.InSlice(s.Charset, sdb.MySQLSupportCharsetList) {
-				return c.NewError(code.InvalidParameter, c.T(`字符集参数无效`)).SetZone(`charset`)
+				return c.NewError(code.InvalidParameter, `字符集参数无效`).SetZone(`charset`)
 			}
 		}
 	}
