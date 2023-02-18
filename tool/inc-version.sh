817774f3959045c62055e734e4868ee2324e7525
Hank Shen <swh@admpub.com>
2023-02-18 17:55:42 +0800

    update

--- tool/inc-version.sh
+++ tool/inc-version.sh
@@ -4,7 +4,7 @@ export ENTRYDIR=${GOPATH}/src
 export WORKDIR=${PWD}
 
 # Go configuration
-export GO_VERSION="1.20"
+export GO_VERSION="1.20.1"
 
 # Nging configuration
 export NGING_VERSION="5.0.0"
