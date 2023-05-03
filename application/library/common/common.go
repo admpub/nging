3c39d2fd2608f7731e0df63d3e91c8b025fac3a7
Hank Shen <swh@admpub.com>
2023-05-03 17:02:55 +0800

    update

--- application/library/common/common.go
+++ application/library/common/common.go
@@ -21,7 +21,6 @@ package common
 import (
 	"errors"
 	"io"
-	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -81,98 +80,6 @@ func SendErr(ctx echo.Context, err error) {
 
 type ConfigFromDB interface {
 	ConfigFromDB() echo.H
-}
-
-// GetLocalIP 获取本机网卡IP
-func GetLocalIP(ver ...int) (string, error) {
-	var v int
-	if len(ver) > 0 {
-		v = ver[0]
-	}
-	if v == 6 {
-		return GetLocalIPv6()
-	}
-
-	return GetLocalIPv4()
-}
-
-// GetLocalIPv4 获取本机网卡IPv4
-func GetLocalIPv4() (ipv4 string, err error) {
-	var (
-		addrs   []net.Addr
-		addr    net.Addr
-		ipNet   *net.IPNet // IP地址
-		isIPNet bool
-	)
-	ipv4 = `127.0.0.1`
-	// 获取所有网卡
-	if addrs, err = net.InterfaceAddrs(); err != nil {
-		return
-	}
-	// 取第一个非lo的网卡IP
-	for _, addr = range addrs {
-		// 这个网络地址是IP地址: ipv4, ipv6
-		if ipNet, isIPNet = addr.(*net.IPNet); isIPNet && !ipNet.IP.IsLoopback() {
-			// 跳过IPV6
-			if ipNet.IP.To4() != nil {
-				ipv4 = ipNet.IP.String() // 192.168.1.1
-				return
-			}
-		}
-	}
-	return
-}
-
-// GetLocalIPv6 获取本机网卡IPv6
-func GetLocalIPv6() (ipv6 string, err error) {
-	var (
-		addrs   []net.Addr
-		addr    net.Addr
-		ipNet   *net.IPNet // IP地址
-		isIPNet bool
-	)
-	ipv6 = `::1`
-	// 获取所有网卡
-	if addrs, err = net.InterfaceAddrs(); err != nil {
-		return
-	}
-	// 取第一个非lo的网卡IP
-	for _, addr = range addrs {
-		// 这个网络地址是IP地址: ipv4, ipv6
-		if ipNet, isIPNet = addr.(*net.IPNet); isIPNet && !ipNet.IP.IsLoopback() {
-			// IPV6
-			if ipNet.IP.To4() == nil {
-				ipv6 = ipNet.IP.String()
-				return
-			}
-		}
-	}
-	return
-}
-
-// GetLocalIPs 获取本机网卡IP
-func GetLocalIPs() (ipv4 []string, ipv6 []string, err error) {
-	var (
-		addrs   []net.Addr
-		addr    net.Addr
-		ipNet   *net.IPNet // IP地址
-		isIPNet bool
-	)
-	// 获取所有网卡
-	if addrs, err = net.InterfaceAddrs(); err != nil {
-		return
-	}
-	for _, addr = range addrs {
-		// 这个网络地址是IP地址: ipv4, ipv6
-		if ipNet, isIPNet = addr.(*net.IPNet); isIPNet && !ipNet.IP.IsLoopback() {
-			if ipNet.IP.To4() != nil {
-				ipv4 = append(ipv4, ipNet.IP.String()) // 192.168.1.1
-			} else {
-				ipv6 = append(ipv6, ipNet.IP.String())
-			}
-		}
-	}
-	return
 }
 
 var notWordRegexp = regexp.MustCompile(`[^\w]+`)
