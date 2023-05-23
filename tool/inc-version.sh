1cee3373e96313c77af2d5a435b079c8f12e55de
Hank Shen <swh@admpub.com>
2023-05-24 00:01:49 +0800

    update

--- tool/inc-version.sh
+++ tool/inc-version.sh
@@ -10,5 +10,5 @@ export GO_VERSION="1.20.4"
 export NGING_VERSION="5.0.1"
 export NGING_BUILD=`date +%Y%m%d%H%M%S`
 export NGING_COMMIT=`git rev-parse HEAD`
-export NGING_LABEL="beta6"
+export NGING_LABEL="beta7"
 export NGING_EXECUTOR="nging"
