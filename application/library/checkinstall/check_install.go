6591756c1d993022f2f56c196c11c2ea9aef3eec
Hank Shen <swh@admpub.com>
2023-06-09 22:31:24 +0800

    update

--- application/library/checkinstall/check_install.go
+++ application/library/checkinstall/check_install.go
@@ -6,19 +6,30 @@ import (
 	"github.com/admpub/once"
 )
 
-func New(name string) *CheckInstall {
-	return &CheckInstall{name: name}
+func New(name string, checker ...func(name string) bool) *CheckInstall {
+	c := &CheckInstall{name: name}
+	if len(checker) > 0 && checker[0] != nil {
+		c.checker = checker[0]
+	} else {
+		c.checker = DefaultChecker
+	}
+	return c
+}
+
+var DefaultChecker = func(name string) bool {
+	_, err := exec.LookPath(name)
+	return err == nil
 }
 
 type CheckInstall struct {
 	name      string
 	supported bool
 	checkonce once.Once
+	checker   func(name string) bool
 }
 
 func (c *CheckInstall) check() {
-	_, err := exec.LookPath(c.name)
-	c.supported = err == nil
+	c.supported = c.checker(c.name)
 }
 
 func (c *CheckInstall) IsSupported() bool {
