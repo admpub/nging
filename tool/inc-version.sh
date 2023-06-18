073a1e732a2f5ea15d7422743c4a01c3639545f7
Hank Shen <swhbox@foxmail.com>
2023-06-18 23:20:08 +0800

    update

--- tool/inc-version.sh
+++ tool/inc-version.sh
@@ -7,8 +7,8 @@ export WORKDIR=${PWD}
 export GO_VERSION="1.20.4"
 
 # Nging configuration
-export NGING_VERSION="5.0.1"
+export NGING_VERSION="5.0.2"
 export NGING_BUILD=`date +%Y%m%d%H%M%S`
 export NGING_COMMIT=`git rev-parse HEAD`
-export NGING_LABEL="stable"
+export NGING_LABEL="beta1"
 export NGING_EXECUTOR="nging"
