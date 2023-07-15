94a25ebfa63c579aafc1c29125f93468007b62c7
Hank Shen <swh@admpub.com>
2023-07-15 22:11:28 +0800

    update

--- tool/nging-builder/main.go
+++ tool/nging-builder/main.go
@@ -12,16 +12,37 @@ import (
 	"strings"
 	"time"
 
+	"github.com/admpub/confl"
 	"github.com/webx-top/com"
 )
 
-var p = buildParam{
+var p = buildParam{}
+
+var c = Config{
 	GoVersion:    `1.20.5`,
 	Executor:     `nging`,
 	NgingVersion: `5.1.0`,
 	NgingLabel:   `stable`,
 	Project:      `github.com/admpub/nging`,
-	WorkDir:      ``,
+	VendorMiscDirs: map[string][]string{
+		`*`: {
+			`vendor/github.com/nging-plugins/caddymanager/template/`,
+			`vendor/github.com/nging-plugins/collector/template/`,
+			`vendor/github.com/nging-plugins/dbmanager/template/`,
+			`vendor/github.com/nging-plugins/dbmanager/public/assets/`,
+			`vendor/github.com/nging-plugins/ddnsmanager/template/`,
+			`vendor/github.com/nging-plugins/dlmanager/template/`,
+			`vendor/github.com/nging-plugins/frpmanager/template/`,
+			`vendor/github.com/nging-plugins/ftpmanager/template/`,
+			`vendor/github.com/nging-plugins/servermanager/template/`,
+			`vendor/github.com/nging-plugins/sshmanager/template/`,
+			`vendor/github.com/nging-plugins/webauthn/template/`,
+		},
+		`linux`: {
+			`vendor/github.com/nging-plugins/firewallmanager/template/`,
+		},
+		`!linux`: {},
+	},
 }
 
 var targetNames = map[string]string{
@@ -37,21 +58,22 @@ var targetNames = map[string]string{
 }
 
 var armRegexp = regexp.MustCompile(`/arm`)
+var configFile = `./builder.conf`
 
 func main() {
-	flag.StringVar(&p.GoVersion, `goVersion`, p.GoVersion, `--goVersion `+p.GoVersion)
-	flag.StringVar(&p.Executor, `executor`, p.Executor, `--executor `+p.Executor)
-	flag.StringVar(&p.NgingVersion, `ngingVersion`, p.NgingVersion, `--ngingVersion `+p.NgingVersion)
-	flag.StringVar(&p.NgingLabel, `ngingLabel`, p.NgingLabel, `--ngingLabel `+p.NgingLabel)
-	flag.StringVar(&p.Project, `project`, p.Project, `--project `+p.Project)
+	flag.StringVar(&configFile, `conf`, configFile, `--conf `+configFile)
 	flag.Parse()
-	var err error
+
+	_, err := confl.DecodeFile(configFile, &c)
+	if err != nil {
+		com.ExitOnFailure(err.Error(), 1)
+	}
+	c.apply()
 	p.ProjectPath, err = com.GetSrcPath(p.Project)
 	if err != nil {
 		com.ExitOnFailure(err.Error(), 1)
 	}
 	p.WorkDir = strings.TrimSuffix(strings.TrimSuffix(p.ProjectPath, `/`), p.Project)
-	fmt.Println(`WorkDir: `, p.WorkDir)
 	var targets []string
 	var armTargets []string
 	addTarget := func(target string, notNames ...bool) {
@@ -73,12 +95,27 @@ func main() {
 		minify = isMinified(os.Args[2])
 		addTarget(os.Args[1])
 	case 2:
-		if isMinified(os.Args[1]) {
+		switch {
+		case isMinified(os.Args[1]):
 			minify = true
 			for _, t := range targetNames {
 				addTarget(t, true)
 			}
-		} else {
+		case os.Args[1] == `genConfig`:
+			b, err := confl.Marshal(c)
+			if err != nil {
+				com.ExitOnFailure(err.Error(), 1)
+			}
+			err = os.WriteFile(configFile, b, os.ModePerm)
+			if err != nil {
+				com.ExitOnFailure(err.Error(), 1)
+			}
+			com.ExitOnSuccess(`successully generate config file: ` + configFile)
+			return
+		case os.Args[1] == `makeGen`:
+			makeGenerateCommandComment(c)
+			return
+		default:
 			addTarget(os.Args[1])
 		}
 	case 1:
@@ -88,6 +125,7 @@ func main() {
 	default:
 		com.ExitOnFailure(`参数错误`)
 	}
+	fmt.Println(`WorkDir: `, p.WorkDir)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	err = os.Chdir(p.ProjectPath)
@@ -280,5 +318,98 @@ func packFiles(p buildParam) {
 	err = os.RemoveAll(p.ReleaseDir)
 	if err != nil {
 		com.ExitOnFailure(err.Error(), 1)
+	}
+}
+
+func genComment(vendorMiscDirs ...string) string {
+	comment := "//go:generate go install github.com/admpub/bindata/v3/go-bindata@latest\n"
+	comment += `//go:generate go-bindata -fs -o bindata_assetfs.go -ignore "\\.(git|svn|DS_Store|less|scss|gitkeep)$" -minify "\\.(js|css)$" -tags bindata`
+	prefixes := []string{}
+	miscDirs := []string{`public/assets/`, `template/`, `config/i18n/`}
+	miscDirs = append(miscDirs, vendorMiscDirs...)
+	uniquePrefixes := map[string]struct{}{}
+	for k, v := range miscDirs {
+		if !strings.HasSuffix(v, `/...`) {
+			if !strings.HasSuffix(v, `/`) {
+				v += `/`
+			}
+			v += `...`
+		}
+		if strings.HasPrefix(v, `vendor/`) {
+			parts := strings.SplitN(v, `/`, 5)
+			if len(parts) == 5 {
+				prefix := strings.Join(parts[0:4], `/`) + `/`
+				if _, ok := uniquePrefixes[prefix]; !ok {
+					uniquePrefixes[prefix] = struct{}{}
+					prefixes = append(prefixes, prefix)
+				}
+			}
+		}
+		miscDirs[k] = v
+	}
+	comment += ` -prefix "` + strings.Join(prefixes, `|`) + `" `
+	comment += strings.Join(miscDirs, ` `)
+	return comment
+}
+
+func makeGenerateCommandComment(c Config) {
+	dfts := c.VendorMiscDirs[`*`]
+	for osName, miscDirs := range c.VendorMiscDirs {
+		if osName == `*` {
+			continue
+		}
+		dirs := make([]string, 0, len(dfts)+len(miscDirs))
+		dirs = append(dirs, dfts...)
+		dirs = append(dirs, miscDirs...)
+		fileName := `main_`
+		if strings.HasPrefix(osName, `!`) {
+			fileName += `non` + strings.TrimPrefix(osName, `!`)
+		} else {
+			fileName += osName
+		}
+		fileName += `.go`
+		filePath := filepath.Join(p.ProjectPath, fileName)
+		fileContent := "//go:build " + osName + "\n\n"
+		fileContent += "package main\n\n"
+		fileContent += genComment(dirs...) + "\n\n"
+		b, err := os.ReadFile(filePath)
+		if err == nil {
+			old := string(b)
+			pos := strings.Index(old, `import `)
+			if pos > -1 {
+				fileContent += old[pos:]
+			}
+		}
+		err = os.WriteFile(filePath, []byte(fileContent), os.ModePerm)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+}
+
+type Config struct {
+	GoVersion      string
+	Executor       string
+	NgingVersion   string
+	NgingLabel     string
+	Project        string
+	VendorMiscDirs map[string][]string // key: GOOS
+}
+
+func (a Config) apply() {
+	if len(a.GoVersion) > 0 {
+		p.GoVersion = a.GoVersion
+	}
+	if len(a.Executor) > 0 {
+		p.Executor = a.Executor
+	}
+	if len(a.NgingVersion) > 0 {
+		p.NgingVersion = a.NgingVersion
+	}
+	if len(a.NgingLabel) > 0 {
+		p.NgingLabel = a.NgingLabel
+	}
+	if len(a.Project) > 0 {
+		p.Project = a.Project
 	}
 }
