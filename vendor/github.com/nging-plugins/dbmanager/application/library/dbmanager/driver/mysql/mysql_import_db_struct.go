e4a239ed9e6476861ab13d2e5583582a172db532
Hank Shen <swhbox@foxmail.com>
2023-07-25 12:48:30 +0800

    update

--- vendor/github.com/nging-plugins/dbmanager/application/library/dbmanager/driver/mysql/mysql_import_db_struct.go
+++ vendor/github.com/nging-plugins/dbmanager/application/library/dbmanager/driver/mysql/mysql_import_db_struct.go
@@ -48,21 +48,28 @@ func execSQLFileWithProgress(
 	exec func(sqlFile string, callback func(strLen int)) func(string) error,
 ) error {
 	fileSize, _ := com.FileSize(sqlFile)
-	var totalPercent int64
+	var remains int64 = 100
+	var partPercent float64
 	var perByteVal float64
 	if fileSize > 0 {
-		perByteVal = float64(100) / float64(fileSize)
+		perByteVal = float64(remains) / float64(fileSize)
 	}
 	var callback = func(strLen int) {
-		if perByteVal > 0 {
-			percent := int64(perByteVal * float64(strLen))
-			totalPercent += percent
-			noticer.Done(percent)
+		if perByteVal <= 0 {
+			return
 		}
+		partPercent += perByteVal * float64(strLen)
+		if partPercent < 1 {
+			return
+		}
+		percent := int64(partPercent)
+		remains -= percent
+		noticer.Done(percent)
+		partPercent = partPercent - float64(percent)
 	}
 	err := com.SeekFileLines(sqlFile, exec(sqlFile, callback))
-	if totalPercent < 100 {
-		noticer.Done(100 - totalPercent)
+	if remains > 0 {
+		noticer.Done(remains)
 	}
 	return err
 }
