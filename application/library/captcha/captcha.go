5d920ddd0225d5c463c3f83193ca62866e0a16b4
Hank Shen <swh@admpub.com>
2023-08-16 12:55:52 +0800

    update

--- application/library/captcha/captcha.go
+++ application/library/captcha/captcha.go
@@ -8,7 +8,7 @@ import (
 
 type ICaptcha interface {
 	// keysValues: key1, value1, key2, value2
-	Render(ctx echo.Context, keysValues ...interface{}) template.HTML
+	Render(ctx echo.Context, templatePath string, keysValues ...interface{}) template.HTML
 	Verify(ctx echo.Context, hostAlias string, name string, captchaIdent ...string) echo.Data
 	MakeData(ctx echo.Context, hostAlias string, name string) echo.H
 }
--- application/library/captcha/default.go
+++ application/library/captcha/default.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 
 	"github.com/admpub/nging/v5/application/library/common"
+	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/middleware/tplfunc"
 )
@@ -14,10 +15,18 @@ type defaultCaptcha struct {
 }
 
 // keysValues: key1, value1, key2, value2
-func (c *defaultCaptcha) Render(ctx echo.Context, keysValues ...interface{}) template.HTML {
+func (c *defaultCaptcha) Render(ctx echo.Context, templatePath string, keysValues ...interface{}) template.HTML {
 	options := tplfunc.MakeMap(keysValues)
 	options.Set("captchaId", common.GetHistoryOrNewCaptchaID(ctx))
-	return tplfunc.CaptchaFormWithURLPrefix(ctx.Echo().Prefix(), options)
+	if len(templatePath) == 0 {
+		return tplfunc.CaptchaFormWithURLPrefix(ctx.Echo().Prefix(), options)
+	}
+
+	b, err := ctx.Fetch(templatePath, options)
+	if err != nil {
+		return template.HTML(err.Error())
+	}
+	return template.HTML(com.Bytes2str(b))
 }
 
 func (c *defaultCaptcha) Verify(ctx echo.Context, hostAlias string, name string, captchaIdent ...string) echo.Data {
