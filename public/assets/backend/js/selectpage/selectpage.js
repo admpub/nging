8a3471f35814787beb507d890a1c1d9e91f9a08a
Hank Shen <swh@admpub.com>
2023-09-14 22:57:03 +0800

    update

--- public/assets/backend/js/selectpage/selectpage.js
+++ public/assets/backend/js/selectpage/selectpage.js
@@ -2044,10 +2044,99 @@
 		tmp = tmp.replace(self.template.tag.textKey, text)
 		tmp = tmp.replace(self.template.tag.valueKey, item.value)
 		tag = $(tmp)
+		tag.prop('draggable',true);
 		tag.data('dataObj', data)
 		if (self.elem.combo_input.prop('disabled')) tag.find('span.tag_close').hide()
 		self.elem.combo_input.closest('li').before(tag)
+		if(!self.elem.element_box.data('inited')){
+			self.elem.element_box.data('inited',true);
+			draggable(self.elem.element_box[0],function(event){
+				self.tagValuesSet(self)
+			});
+		}
 	}
+
+	SelectPage.prototype.draggable = draggable;
+
+function draggable(node,ondrop) {
+	//var node = document.querySelector("#container")
+	var draging = null
+	node.ondragstart = function(event) {draging = event.target}
+	node.ondragover = function(event) {
+		event.preventDefault()
+		var target = event.target
+		if (target.nodeName === "LI" && target !== draging) {
+			// 获取初始位置
+			var targetRect = target.getBoundingClientRect()
+			var dragingRect = draging.getBoundingClientRect()
+			// 判断是否动画元素
+			if (target.animated) return;
+			if (_index(draging) < _index(target)) {
+				// 目标比元素大，插到其后面
+				// nextSibling下一个兄弟元素
+				target.parentNode.insertBefore(draging, target.nextSibling)
+			} else {
+				// 目标比元素小，插到其前面
+				target.parentNode.insertBefore(draging, target)
+			}
+			_animate(dragingRect, draging)
+			_animate(targetRect, target)
+		}
+	}
+	if(ondrop) node.ondrop = ondrop;
+	// 获取元素在父元素中的index
+	function _index(el) {
+		var index = 0
+		if (!el || !el.parentNode) {
+			return -1
+		}
+		// previousElementSibling：上一个兄弟元素
+		while (el && (el = el.previousElementSibling)) {
+			index++
+		}
+		return index
+	}
+	// 触发动画
+	function _animate(prevRect, target) {
+		var ms = 300
+		if (!ms) return;
+		var currentRect = target.getBoundingClientRect()
+		if (prevRect.nodeType === 1) prevRect = prevRect.getBoundingClientRect()
+		_css(target, 'transition', 'none')
+		_css(target, 'transform', 'translate3d(' +
+			(prevRect.left - currentRect.left) + 'px,' +
+			(prevRect.top - currentRect.top) + 'px,0)'
+		);
+		target.offsetWidth; // 触发重绘
+		_css(target, 'transition', 'all ' + ms + 'ms');
+		_css(target, 'transform', 'translate3d(0,0,0)');
+		// 事件到了之后把transition和transform清空
+		clearTimeout(target.animated);
+		target.animated = setTimeout(function() {
+			_css(target, 'transition', '');
+			_css(target, 'transform', '');
+			target.animated = false;
+		}, ms);
+	}
+	// 给元素添加style
+	function _css(el, prop, val) {
+		var style = el && el.style
+		if (!style) return;
+		if (val === void 0) {
+			if (document.defaultView && document.defaultView.getComputedStyle) {
+				val = document.defaultView.getComputedStyle(el, '')
+			} else if (el.currentStyle) {
+				val = el.currentStyle
+			}
+			return prop === void 0 ? val : val[prop]
+		} 
+		if (!(prop in style)) {
+			prop = '-webkit-' + prop;
+		}
+		style[prop] = val + (typeof val === 'string' ? '' : 'px')
+	}
+}
+
 	/**
 	 * Remove a tag in multiple selection mode
 	 * @param {Object} self
