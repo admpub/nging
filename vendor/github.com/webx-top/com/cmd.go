c6db3000ba465b5fc051ea84713a0b3d88059136
Hank Shen <swh@admpub.com>
2023-09-25 15:09:38 +0800

    update

--- vendor/github.com/webx-top/com/cmd.go
+++ vendor/github.com/webx-top/com/cmd.go
@@ -167,12 +167,20 @@ func ParseArgs(command string) (params []string) {
 		params = append(params, string(item))
 	}
 	for k, v := range params {
-		v = envWin.ReplaceAllStringFunc(v, getWinEnv)
-		params[k] = envOS.ReplaceAllStringFunc(v, getEnv)
+		v = ParseWindowsEnvVar(v)
+		params[k] = ParseEnvVar(v)
 	}
 	//fmt.Printf("---> %#v\n", params)
 	//params = []string{}
 	return
+}
+
+func ParseEnvVar(v string) string {
+	return envOS.ReplaceAllStringFunc(v, getEnv)
+}
+
+func ParseWindowsEnvVar(v string) string {
+	return envWin.ReplaceAllStringFunc(v, getWinEnv)
 }
 
 func getWinEnv(s string) string {
@@ -191,14 +199,14 @@ type CmdResultCapturer struct {
 	Do func([]byte) error
 }
 
-func (this CmdResultCapturer) Write(p []byte) (n int, err error) {
-	err = this.Do(p)
+func (c CmdResultCapturer) Write(p []byte) (n int, err error) {
+	err = c.Do(p)
 	n = len(p)
 	return
 }
 
-func (this CmdResultCapturer) WriteString(p string) (n int, err error) {
-	err = this.Do([]byte(p))
+func (c CmdResultCapturer) WriteString(p string) (n int, err error) {
+	err = c.Do([]byte(p))
 	n = len(p)
 	return
 }
