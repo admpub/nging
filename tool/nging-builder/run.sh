fa33b1cf53ae0249a6ddf8dd3bba86797b65e147
Hank Shen <swh@admpub.com>
2023-10-21 10:16:31 +0800

    update

--- tool/nging-builder/run.sh
+++ tool/nging-builder/run.sh
@@ -1,2 +1,6 @@
 go install github.com/admpub/nging-builder@latest
-nging-builder --conf ./builder.conf min
+platform=""
+if [ "$1" != "" ]; then
+    platform="$1"
+fi
+nging-builder --conf ./builder.conf $platform min
