57a0f33480ba05b580525c326d8654ebee07eb9d
Hank Shen <swh@admpub.com>
2023-12-17 19:38:26 +0800

    update

--- application/library/selfupdate/example/instance/main.go
+++ application/library/selfupdate/example/instance/main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/webx-top/com"
+	"time"
 )
 
 func main() {
 	fmt.Println(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~1`)
-	shutdown := make(chan os.Signal, 1)
-	// ctrl+c信号os.Interrupt
-	// pkill信号syscall.SIGTERM
-	signal.Notify(
-		shutdown,
-		os.Interrupt, syscall.SIGTERM,
-	)
-	fmt.Println(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2`)
-	for i := 0; true; i++ {
-		sig := <-shutdown
-		fmt.Printf(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>%s`+com.StrLF, sig)
-		err := os.WriteFile(`./sig.txt`, []byte(sig.String()), os.ModePerm)
-		if err != nil {
-			log.Println(err)
-		}
-	}
+	// shutdown := make(chan os.Signal, 1)
+	// // ctrl+c信号os.Interrupt
+	// // pkill信号syscall.SIGTERM
+	// signal.Notify(
+	// 	shutdown,
+	// 	os.Interrupt, syscall.SIGTERM, syscall.SIGKILL, syscall.SIGHUP,
+	// )
+	// fmt.Println(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~2`)
+	// for i := 0; true; i++ {
+	// 	fmt.Printf(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>%s`+com.StrLF, `A`)
+	// 	sig := <-shutdown
+	// 	fmt.Printf(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>%s`+com.StrLF, sig)
+	// 	err := os.WriteFile(`./sig.txt`, []byte(sig.String()), os.ModePerm)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// }
 	fmt.Println(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~3`)
+	time.Sleep(time.Hour)
 }
