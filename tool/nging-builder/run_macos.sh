915a4ad5467b340f72c9fc1b2ad9473266079a73
Hank Shen <swh@admpub.com>
2023-12-18 12:34:32 +0800

    update

--- tool/nging-builder/run_macos.sh
+++ tool/nging-builder/run_macos.sh
@@ -1,2 +1,6 @@
 go install github.com/admpub/nging-builder@latest
+if [ "$1" != "" ]; then
+nging-builder --conf ./builder-$1.conf darwin_amd64 min
+exit 0
+fi
 nging-builder --conf ./builder.conf darwin_amd64 min
