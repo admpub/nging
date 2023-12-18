44b968cdd443ce9d0c40fde0277903d1dd95c4bd
Hank Shen <swh@admpub.com>
2023-12-19 02:34:02 +0800

    update

--- application/library/service/default.go
+++ application/library/service/default.go
@@ -30,14 +30,13 @@ import (
 
 func Run(options *Options, action string) error {
 	conf := &Config{}
-	conf.CopyFromOptions(options)
 	conf.Dir = com.SelfDir()
 	conf.Exec = filepath.Join(conf.Dir, `startup`) //os.Args[0]
 	conf.Env = os.Environ()
-	conf.Executable = conf.Exec
 	if len(os.Args) > 3 {
 		conf.Args = os.Args[3:]
 	}
+	conf.CopyFromOptions(options)
 	if err := initServiceLog(conf); err != nil {
 		return err
 	}
