bc140ebcb0c20044a2c1a4f6d537615a24af869a
Hank Shen <swh@admpub.com>
2024-04-02 13:49:00 +0800

    update

--- public/assets/backend/js/editor/editor.js
+++ public/assets/backend/js/editor/editor.js
@@ -920,23 +920,16 @@ App.editor.codemirror = function (elem,options,loadMode) {
 		var option = $.extend(defaults, options || {});
 		var editor = $(elem)[0].tagName.toUpperCase()=='TEXTAREA' ? CodeMirror.fromTextArea($(elem)[0], option) : CodeMirror($(elem)[0], option);
 		//editor.setSize('auto', 'auto');
-		var mime = option.mode;
+		var mime = option.mode, type2mime = {"text/x-csrc":"clike","text/css":"css","text/x-mysql":"sql","text/x-mssql":"sql","text/x-markdown":"markdown","text/x-yaml":"yaml","text/x-toml":"toml","text/javascript":"javascript","application/javascript":"javascript","text/json":"javascript","text/html":"html","message/http":"http","null":"null"};
+		if(loadMode && typeof(loadMode)=='object') {
+			type2mime = $.extend(type2mime,loadMode);
+			loadMode = null;
+		}
 		if(!loadMode){
-			switch(option.mode){
-				case "text/x-csrc": loadMode = "clike";break;
-				case "text/css": loadMode = "css";break;
-				case "text/x-mysql": loadMode = "sql";break;
-				case "text/x-mssql": loadMode = "sql";break;
-				case "text/x-markdown": loadMode = "markdown";break;
-				case "text/x-yaml": loadMode = "yaml";break;
-				case "text/x-toml": loadMode = "toml";break;
-				case "text/javascript": loadMode = "javascript";break;
-				case "application/javascript": loadMode = "javascript";break;
-				case "text/json": loadMode = "javascript";break;
-				case "text/html": loadMode = "html";break;
-				case "message/http": loadMode = "http";break;
-				case "null": loadMode = "null";break;
-				default: if(typeof(CodeMirror.modeInfo)!=='undefined'){
+			if(option.mode in type2mime){
+				loadMode=type2mime[option.mode];
+			}else{
+				if(typeof(CodeMirror.modeInfo)!=='undefined'){
 					for(var i = 0; i < CodeMirror.modeInfo.length; i++){
 						var v = CodeMirror.modeInfo[i];
 						if(v.mime == option.mode || v.mode == option.mode){
