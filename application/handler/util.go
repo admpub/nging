3cb2ce0afb609b20f7b04892708996dc16bdfeb8
Hank Shen <swh@admpub.com>
2024-07-03 16:46:03 +0800

    update

--- application/handler/util.go
+++ application/handler/util.go
@@ -144,11 +144,17 @@ func NoticeWriter(ctx echo.Context, noticeType string) (wOut io.Writer, wErr io.
 	return
 }
 
-func URLFor(purl string) string {
+func URLFor(purl string, relative ...bool) string {
+	if len(relative) > 0 && relative[0] {
+		return subdomains.Default.RelativeURL(purl, `backend`)
+	}
 	return subdomains.Default.URL(purl, `backend`)
 }
 
-func FrontendURLFor(purl string) string {
+func FrontendURLFor(purl string, relative ...bool) string {
+	if len(relative) > 0 && relative[0] {
+		return subdomains.Default.RelativeURL(purl, `frontend`)
+	}
 	return subdomains.Default.URL(purl, `frontend`)
 }
 
