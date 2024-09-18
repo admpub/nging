8c99f8281728d9709ca8422a5f4712a7def9681c
Hank Shen <swh@admpub.com>
2024-09-18 14:10:25 +0800

    update

--- tool/minify/cleanCSS.sh
+++ tool/minify/cleanCSS.sh
@@ -2,6 +2,7 @@ go install github.com/daaku/cssdalek@latest
 baseDir=../../
 cssdalek \
   --css "${baseDir}public/assets/backend/js/bootstrap/dist/css/bootstrap.css"\
+  --word "${baseDir}public/assets/backend/js/bootstrap/dist/js/bootstrap.js"\
   --word "${baseDir}template/backend/*.html"\
   --word "${baseDir}template/backend/*/*.html"\
   --word "${baseDir}template/backend/*/*/*.html"\
@@ -21,8 +22,7 @@ cssdalek \
   --word "${baseDir}../webx/template/backend/official/*/*/*.html"\
   --word "${baseDir}public/assets/backend/js/behaviour/*.js"\
   --word "${baseDir}public/assets/backend/js/behaviour/*/*.js"\
-  --include-class "switch-*"\
-  --include-selector ".has-switch span.switch-success.switch-left + label"\
-  --include-selector ".has-switch .switch-off span.switch-left + label"\
+  --word "${baseDir}public/assets/backend/js/bootstrap/dist/js/bootstrap.js"\
+  --word "${baseDir}public/assets/backend/js/bootstrap.switch/bootstrap-switch.min.js"\
   --include-selector ".profile_menu .dropdown-toggle"\
   --include-id "captchaImage" > ${baseDir}public/assets/backend/css/style.lite.min.css
