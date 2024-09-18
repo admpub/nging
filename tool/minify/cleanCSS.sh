0df48e74be97b5d4d5dbdbb67197d96425748efe
Hank Shen <swh@admpub.com>
2024-09-18 16:28:32 +0800

    update

--- tool/minify/cleanCSS.sh
+++ tool/minify/cleanCSS.sh
@@ -24,6 +24,8 @@ cssdalek \
   --word "${baseDir}public/assets/backend/js/behaviour/*/*.js"\
   --word "${baseDir}public/assets/backend/js/bootstrap/dist/js/bootstrap.js"\
   --word "${baseDir}public/assets/backend/js/bootstrap.switch/bootstrap-switch.min.js"\
+  --word "${baseDir}public/assets/backend/js/jquery.select2/select2.js"\
+  --word "${baseDir}public/assets/backend/js/jquery.parsley/parsley.js"\
   --include-selector ".profile_menu .dropdown-toggle"\
   --include-selector ".code-cont .main-app"\
   --include-id "captchaImage" > ${baseDir}public/assets/backend/css/style.lite.min.css
