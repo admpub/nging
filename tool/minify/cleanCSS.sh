12ae36dd58f10fa3bcd243cda1d7d0f9b009849d
Hank Shen <swh@admpub.com>
2024-09-18 14:02:46 +0800

    update

--- tool/minify/cleanCSS.sh
+++ tool/minify/cleanCSS.sh
@@ -1,17 +1,28 @@
 go install github.com/daaku/cssdalek@latest
+baseDir=../../
 cssdalek \
-  --css '../../public/assets/backend/js/bootstrap/dist/css/bootstrap.css'\
-  --word '../../template/backend/*.html'\
-  --word '../../template/backend/*/*.html'\
-  --word '../../template/backend/*/*/*.html'\
-  --word '../../public/assets/backend/js/behaviour/*.js'\
-  --word '../../public/assets/backend/js/behaviour/*/*.js' > ../../public/assets/backend/js/bootstrap/dist/css/bootstrap.lite.min.css
+  --css "${baseDir}public/assets/backend/js/bootstrap/dist/css/bootstrap.css"\
+  --word "${baseDir}template/backend/*.html"\
+  --word "${baseDir}template/backend/*/*.html"\
+  --word "${baseDir}template/backend/*/*/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*/*/*.html"\
+  --word "${baseDir}public/assets/backend/js/behaviour/*.js"\
+  --word "${baseDir}public/assets/backend/js/behaviour/*/*.js" > ${baseDir}public/assets/backend/js/bootstrap/dist/css/bootstrap.lite.min.css
 
 cssdalek \
-  --css '../../public/assets/backend/css/style.css'\
-  --word '../../template/backend/*.html'\
-  --word '../../template/backend/*/*.html'\
-  --word '../../template/backend/*/*/*.html'\
-  --word '../../public/assets/backend/js/behaviour/*.js'\
-  --word '../../public/assets/backend/js/behaviour/*/*.js'\
-  --include-id 'captchaImage' > ../../public/assets/backend/css/style.lite.min.css
+  --css "${baseDir}public/assets/backend/css/style.css"\
+  --word "${baseDir}template/backend/*.html"\
+  --word "${baseDir}template/backend/*/*.html"\
+  --word "${baseDir}template/backend/*/*/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*/*.html"\
+  --word "${baseDir}../webx/template/backend/official/*/*/*.html"\
+  --word "${baseDir}public/assets/backend/js/behaviour/*.js"\
+  --word "${baseDir}public/assets/backend/js/behaviour/*/*.js"\
+  --include-class "switch-*"\
+  --include-selector ".has-switch span.switch-success.switch-left + label"\
+  --include-selector ".has-switch .switch-off span.switch-left + label"\
+  --include-selector ".profile_menu .dropdown-toggle"\
+  --include-id "captchaImage" > ${baseDir}public/assets/backend/css/style.lite.min.css
