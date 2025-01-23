ef49600ee561f8feabbbcd2ee1c5841847dc70d5
Hank Shen <swh@admpub.com>
2025-01-23 18:08:48 +0800

    update

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ var (
 	CLOUD_GOX  string
 	COMMIT     string
 	LABEL      = `dev` //beta/alpha/stable
-	VERSION    = `5.2.7`
+	VERSION    = `5.2.8`
 	PACKAGE    = `free`
 
 	schemaVer = version.DBSCHEMA //数据表结构版本
