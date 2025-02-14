ee923a3351f9a92f7ae7033712b83a99200a1a49
Hank Shen <swh@admpub.com>
2025-02-14 14:01:19 +0800

    update

---
+++ tool/builder/build.sh
@@ -0,0 +1,10 @@
+GO_VERSION=1.24.0
+if [ "$1" != "" ];then
+    GO_VERSION="$1"
+fi
+docker build -t admpub/xgo:${GO_VERSION} . --build-arg GO_VERSION=${GO_VERSION}
+if [ "$2" == "push" ];then
+    docker push admpub/xgo:${GO_VERSION}
+    #docker tag admpub/xgo:${GO_VERSION} admpub/xgo:latest
+    #docker push admpub/xgo:latest
+fi
