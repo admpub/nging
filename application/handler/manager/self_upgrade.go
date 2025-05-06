b88d7f34d128dec778c7b10d52418768bde2976d
Hank Shen <swh@admpub.com>
2025-05-06 17:40:18 +0800

    update

--- application/handler/manager/self_upgrade.go
+++ application/handler/manager/self_upgrade.go
@@ -1,11 +1,20 @@
 package manager
 
 import (
+	"context"
+	"fmt"
+	"mime/multipart"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/coscms/webcore/cmd"
+	"github.com/coscms/webcore/library/backend"
 	"github.com/coscms/webcore/library/config"
 	"github.com/coscms/webcore/library/license"
+	"github.com/coscms/webcore/library/notice"
+	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/code"
 	"github.com/webx-top/echo/param"
@@ -19,6 +28,12 @@ func selfUpgrade(ctx echo.Context) error {
 	if config.FromFile().Settings().Debug && ctx.Formx(`restart`).Bool() { // url: /manager/upgrade?restart=true
 		selfExit()
 		return nil
+	}
+	if ctx.Formx(`upload`).Bool() {
+		if ctx.IsPost() {
+			return selfUpgradeUpload(ctx)
+		}
+		return ctx.Render(`manager/upgrade`, nil)
 	}
 	data := ctx.Data()
 	if ctx.Formx(`local`).Bool() { // url: /manager/upgrade?local=true
@@ -78,4 +93,50 @@ func selfUpgrade(ctx echo.Context) error {
 		`isNew`:  info.IsNew(),
 		`nonce`:  nonce,
 	}))
+}
+
+func selfUpgradeUpload(ctx echo.Context) error {
+	var username string
+	user := backend.User(ctx)
+	if user != nil {
+		username = user.Username
+	}
+	pv := &license.ProductVersion{}
+	np := notice.NewP(ctx, `ngingUploadNewVersion`, username, context.Background()).AutoComplete(true)
+	pv.SetProgressor(np)
+	saveTo := filepath.Join(echo.Wd(), `data/cache/nging-new-version`, `upload`)
+	err := com.MkdirAll(saveTo, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	var newFileName string
+	_, err = ctx.SaveUploadedFile(`file`, saveTo, func(fh *multipart.FileHeader) (string, error) {
+		newFileName = filepath.Base(fh.Filename)
+		extension := strings.ToLower(filepath.Ext(fh.Filename))
+		if extension != `.gz` && extension != `.tar` && extension != `.tar.gz` {
+			return newFileName, fmt.Errorf(`sorry, uploading this file is not supported. only files with the extension “%s” can be uploaded`, `.tar.gz`)
+		}
+		return newFileName, nil
+	})
+	if err != nil {
+		return err
+	}
+	saveTo += echo.FilePathSeparator + newFileName
+	pv.SetDownloadedPath(saveTo)
+	err = pv.Extract()
+	if err != nil {
+		return err
+	}
+	err = pv.Upgrade(ctx, echo.Wd(), `none`)
+	if err != nil {
+		return err
+	}
+	go func() {
+		time.Sleep(time.Millisecond * 500)
+		selfExit()
+	}()
+	data := ctx.Data()
+	data.SetData(echo.H{`newVersion`: pv.Version})
+	data.SetInfo(ctx.T(`上传成功，正在升级请稍候...`), code.Success.Int())
+	return ctx.JSON(data)
 }
