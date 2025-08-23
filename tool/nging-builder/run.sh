3016b54afec4fadd0d40fad92f1a9fd33aa73411
Hank Shen <swhbox@foxmail.com>
2025-08-23 11:48:13 +0800

    update

--- tool/nging-builder/run.sh
+++ tool/nging-builder/run.sh
@@ -1,4 +1,27 @@
-go install github.com/admpub/nging-builder@latest
+# nging-builder
+if which nging-builder &> /dev/null; then
+    echo "found nging-builder"
+else
+    echo "installing nging-builder"
+    go install github.com/admpub/nging-builder@latest
+fi
+
+# xgo
+if which xgo &> /dev/null; then
+    echo "found xgo"
+else
+    echo "installing xgo"
+    go install github.com/admpub/xgo@latest
+fi
+
+# go-bindata
+if which go-bindata &> /dev/null; then
+    echo "found go-bindata"
+else
+    echo "installing go-bindata"
+    go install github.com/admpub/bindata/v3/go-bindata@latest
+fi
+
 platform=""
 if [ "$1" != "" ]; then
     platform="$1"
