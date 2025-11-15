7d546c38952c04fd6b0d6f40e46a0b12d2962ab4
Hank Shen <swhbox@foxmail.com>
2025-11-15 13:23:31 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
 NGING_BUILDER_PATH=./tool/nging-builder
 # * build *
-#cd $NGING_BUILDER_PATH && ./run.sh linux_386,linux_amd64,linux_arm64 && cd ../../
+cd $NGING_BUILDER_PATH && ./run.sh linux_386,linux_amd64,linux_arm64 && cd ../../
 buikdkit=`docker images | grep moby/buildkit | awk '{print $1}' | head -n 1`
 if [ "$buikdkit" = "" ];then
     docker buildx create --name container-builder --driver docker-container --use --bootstrap
