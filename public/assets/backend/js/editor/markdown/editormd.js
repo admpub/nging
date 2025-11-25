bf199ad5e0bf1baca02dd2116b156d6fbb378465
Hank Shen <swhbox@foxmail.com>
2025-11-25 12:39:39 +0800

    update

--- public/assets/backend/js/editor/markdown/editormd.js
+++ public/assets/backend/js/editor/markdown/editormd.js
@@ -4058,22 +4058,7 @@
     
     // CodeMirror / editor area themes
     // @1.5.0 rename -> editorThemes, old version -> themes
-    editormd.editorThemes = [
-        "default", "3024-day", "3024-night",
-        "ambiance", "ambiance-mobile",
-        "base16-dark", "base16-light", "blackboard",
-        "cobalt",
-        "eclipse", "elegant", "erlang-dark",
-        "lesser-dark",
-        "mbo", "mdn-like", "midnight", "monokai",
-        "neat", "neo", "night",
-        "paraiso-dark", "paraiso-light", "pastel-on-dark",
-        "rubyblue",
-        "solarized",
-        "the-matrix", "tomorrow-night-eighties", "twilight",
-        "vibrant-ink",
-        "xq-dark", "xq-light"
-    ];
+    editormd.editorThemes = ["ambiance", "ambiance-mobile","night"];
 
     editormd.loadPlugins = {};
     
