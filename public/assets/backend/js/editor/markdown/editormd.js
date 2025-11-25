b68a24e6ff66ea7b072b4de280f1e69c5ddcfdc3
Hank Shen <swhbox@foxmail.com>
2025-11-25 12:06:59 +0800

    update

--- public/assets/backend/js/editor/markdown/editormd.js
+++ public/assets/backend/js/editor/markdown/editormd.js
@@ -1862,7 +1862,7 @@
             return this;
         },
         toolbarHeight: function() {
-            return this.toolbartoolbar.height()||37
+            return this.toolbar.height()||37
         },
         /**
          * 调整编辑器的尺寸和布局
