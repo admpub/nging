f7c5c9873233bc9a4d24055d2023b0acaf50f534
Hank Shen <swhbox@foxmail.com>
2025-12-27 17:34:37 +0800

    update

--- public/assets/backend/js/behaviour/page/login.js
+++ public/assets/backend/js/behaviour/page/login.js
@@ -1,12 +1,12 @@
 $(function(){
     $('[data-toggle="tooltip"]').tooltip();
     submitEncryptedData('#login-form');
-    $('#login-form').find('input[name=remember]').on('click', function(){
+    $('#login-form').find('input[name=remember]').on('change', function(){
         var checked = $(this).prop('checked');
         if(checked){
             setCookie('RememberBackendLogin', $(this).val(), 30, `/`);
         }else{
             setCookie('RememberBackendLogin', '', -1, `/`);
         }
-    }).trigger('click');
+    }).trigger('change');
 });
