44b01ca474cef508d8bbdd6045ec466b1acba9c8
Hank Shen <swhbox@foxmail.com>
2026-03-01 20:54:50 +0800

    update

--- public/assets/backend/js/behaviour/page/cascade-select.js
+++ public/assets/backend/js/behaviour/page/cascade-select.js
@@ -11,15 +11,16 @@
       factory(jQuery);
   }
 }(function ($) {
-function fetchOptions(elem,pid,selectedId,selectedIds,url){
+function fetchOptions(elem,pid,selectedId,selectedIds,url,idKey,callback){
   if(!url) url=BACKEND_URL+'/tool/area/index';
-  $.get(url,{pid:pid},function(r){
+  $.get(typeof url === 'function'?url():url,{pid:pid},function(r){
       if(r.Code!=1){
         return App.message({text:r.Info,type:'error'});
       }
-      if(r.Data.listData.length<1){
+      if(!r.Data || r.Data.listData.length<1){
         while($(elem).next('select').length>0) $(elem).next('select').remove();
-        $(elem).remove();
+        if(pid!=''&&pid!='0') $(elem).remove();
+        else $(elem).html('<option value=""> - '+(App.i18n.PLEASE_SELECT?App.i18n.PLEASE_SELECT:'请选择')+' - </option>');
         return;
       }
       $(elem).show(h);
@@ -27,15 +28,17 @@ function fetchOptions(elem,pid,selectedId,selectedIds,url){
       var h = '<option value=""> - '+(App.i18n.PLEASE_SELECT?App.i18n.PLEASE_SELECT:'请选择')+' - </option>';
       for(var i=0;i<r.Data.listData.length;i++){
         var v=r.Data.listData[i];
-        var s=selectedId==v.id?' selected':'';
-        if(exclude==v.id) s+=' disabled';
-        h+='<option value="'+v.id+'"'+s+'>'+v.name+'</option>';
+        var s='';
+        if(selectedId==v[idKey]) s+=' selected';
+        if(exclude==v[idKey]) s+=' disabled';
+        h+='<option value="'+v[idKey]+'"'+s+'>'+v.name+'</option>';
       }
       $(elem).html(h);
-      bindEvent(elem,selectedIds,url).trigger('change');
+      callback && callback(pid,r);
+      bindEvent(elem,selectedIds,url,idKey,callback).trigger('change');
   },'json');
 }
-function bindEvent(elem,selectedIds,url){
+function bindEvent(elem,selectedIds,url,idKey,callback){
   if($(elem).data('bindEvent')){
     return $(elem);
   }
@@ -64,19 +67,57 @@ function bindEvent(elem,selectedIds,url){
       }
       var selectedId='';
       if($.isArray(selectedIds) && selectedIds.length>index) selectedId=selectedIds[index];
-      fetchOptions($(this).next('select')[0],v,selectedId,selectedIds,url);
+      fetchOptions($(this).next('select')[0],v,selectedId,selectedIds,url,idKey,callback);
   });
 }
-function init(elem,selectedIds,url){
+function init(elem,selectedIds,url,idKey,callback){
   if(selectedIds == null){
     selectedIds = $(elem).data('selected') || [];
     if(selectedIds && !$.isArray(selectedIds)) selectedIds = String(selectedIds).split(",");
   }
-  if(url == null){
-    url = $(elem).data('url');
-  }
+  if(!url) url = $(elem).data('url');
+  if(!idKey) idKey = $(elem).data('idKey') || 'id';
   var selected = selectedIds && selectedIds.length > 0 ? selectedIds[0] : '';
-  fetchOptions(elem,0,selected,selectedIds,url);
+  var country = $(elem).data('country');
+  var countryURL = $(elem).data('country-url');
+  if(country || countryURL){
+    if(!countryURL) countryURL = typeof url == 'function' ? url() : url;
+    $.get(countryURL,{country:true,id:selected},function(r){
+        if(r.Code!=1){
+          return App.message({text:r.Info,type:'error'});
+        }
+        if(!r.Data || r.Data.listData.length<1){
+          return;
+        }
+        var h = '<option value=""> - '+(App.i18n.PLEASE_SELECT?App.i18n.PLEASE_SELECT:'请选择')+' - </option>';
+        var selectedAbbr = r.Data.countryAbbr||'';
+        for(var i=0;i<r.Data.listData.length;i++){
+          var v=r.Data.listData[i];
+          var s='';
+          if(selectedAbbr==v.abbr) s+=' selected';
+          h+='<option value="'+v.abbr+'"'+s+'>'+v.name+'</option>';
+        }
+        var wrap=$(elem).prev('span.select-country-wrap');
+        if(wrap.length<1){
+          var $h=$('<span class="select-country-wrap"><select id="select-country">'+h+'</select></span>');
+          $(elem).before($h);
+          var $select=$h.children('select');
+          $select.attr('class',$(elem).attr('class'));
+          $select.on('change',function(){
+            $(elem).trigger('reload')
+          }).trigger('change');
+        }
+    },'json');
+    var oldURL = typeof url == 'function' ? url() : url;
+    var sep = oldURL.indexOf('?') > 0 ? '&' : '?';
+    url = function(){
+      return oldURL + sep + 'countryAbbr='+($('#select-country').val()||'');
+    };
+  }
+  fetchOptions(elem,0,selected,selectedIds,url,idKey,callback);
+  $(elem).on('reload',function(){
+    fetchOptions(elem,0,selected,selectedIds,url,idKey,callback);
+  });
 }
 window.CascadeSelect = {
   init: init
