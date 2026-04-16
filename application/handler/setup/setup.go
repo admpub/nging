bc5a0e43a8acc45db7aa8c9539608a1c15c1b94f
Hank Shen <swhbox@foxmail.com>
2026-04-16 14:05:31 +0800

    update

--- application/handler/setup/setup.go
+++ application/handler/setup/setup.go
@@ -46,51 +46,6 @@ import (
 	"github.com/coscms/webcore/request"
 )
 
-type ProgressInfo struct {
-	Finished  int64
-	TotalSize int64
-	Summary   string
-	Timestamp int64
-	mu        *sync.RWMutex
-}
-
-func (p *ProgressInfo) Clone() ProgressInfo {
-	p.mu.RLock()
-	r := *p
-	p.mu.RUnlock()
-	return r
-}
-
-func (p *ProgressInfo) GetTs() int64 {
-	p.mu.RLock()
-	r := p.Timestamp
-	p.mu.RUnlock()
-	return r
-}
-
-func (p *ProgressInfo) SetTs(ts int64) {
-	p.mu.Lock()
-	p.Timestamp = ts
-	p.mu.Unlock()
-}
-
-func (p *ProgressInfo) Done(n int64) {
-	p.mu.Lock()
-	newVal := p.Finished + n
-	if newVal > p.TotalSize {
-		p.Finished = p.TotalSize
-	} else {
-		p.Finished = newVal
-	}
-	p.mu.Unlock()
-}
-
-func (p *ProgressInfo) Add(n int64) {
-	p.mu.Lock()
-	p.TotalSize += n
-	p.mu.Unlock()
-}
-
 var (
 	installProgress   = &ProgressInfo{mu: &sync.RWMutex{}}
 	installedProgress = &ProgressInfo{
@@ -165,6 +120,11 @@ func Setup(ctx echo.Context) error {
 			return ctx.NewError(stdCode.Failure, err.Error())
 		}
 		installProgress.SetTs(time.Now().Unix())
+		defer func() {
+			if err != nil {
+				installProgress.Reset()
+			}
+		}()
 		installSQLs := config.GetInstallSQLs()
 		insertSQLs := config.GetInsertSQLs()
 		var totalSize int64
